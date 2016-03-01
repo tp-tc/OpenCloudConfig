7786bebc2181d507753d581ea48ca3f1cfaca2de
Rob Thijssen <rthijssen@gmail.com>
2016-03-01 17:43:31 +0000

    testing

--- userdata/try-win2012-vs2013/SoftwareConfig.ps1
+++ userdata/try-win2012-vs2013/SoftwareConfig.ps1
@@ -29,7 +29,6 @@ Configuration SoftwareConfig {
   Package RustInstall {
     Name = 'rust-beta-x86_64-pc-windows-msvc'
     Path = 'https://static.rust-lang.org/dist/rust-beta-x86_64-pc-windows-msvc.msi'
-    Ensure = 'Present'
     LogPath = ('{0}\log' -f $env:SystemDrive)
   }
   #Package MozillaBuildInstall {
