82996551db8713bddea17c33b1eaf7da6d278001
Rob Thijssen <rthijssen@gmail.com>
2016-03-01 18:54:47 +0000

    testing

--- userdata/try-win2012-vs2013/ServiceConfig.ps1
+++ userdata/try-win2012-vs2013/ServiceConfig.ps1
@@ -1,6 +1,7 @@
 
 # ServiceConfig installs, removes, disables or enables Windows Services
 Configuration ServiceConfig {
+  Import-DscResource -ModuleName PSDesiredStateConfiguration
   Service UpdateDisable {
     Name = 'wuauserv'
     State = 'Stopped'
