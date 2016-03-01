84ae514734b0b0faf0969f6f41adaa0d4843d7ad
Rob Thijssen <rthijssen@gmail.com>
2016-03-01 18:11:23 +0000

    testing

--- userdata/try-win2012-vs2013/FeatureConfig.ps1
+++ userdata/try-win2012-vs2013/FeatureConfig.ps1
@@ -2,7 +2,7 @@
 # FeatureConfig installs or removes operating system Windows Features
 Configuration FeatureConfig {
   WindowsFeature DotNetFrameworkInstall {
-    Name = NET-Framework-Core
-    Ensure = Present
+    Name = 'NET-Framework-Core'
+    Ensure = 'Present'
   }
 }
