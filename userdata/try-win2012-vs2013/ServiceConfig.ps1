7fe2a40cde1c28f1e96cb7cedf3201fd6b3d0283
Rob Thijssen <rthijssen@gmail.com>
2016-03-01 18:23:31 +0000

    testing

--- userdata/try-win2012-vs2013/ServiceConfig.ps1
+++ userdata/try-win2012-vs2013/ServiceConfig.ps1
@@ -1,17 +1,9 @@
+
+# ServiceConfig installs, removes, disables or enables Windows Services
 Configuration ServiceConfig {
   Service UpdateDisable {
     Name = 'wuauserv'
     State = 'Stopped'
     StartupType = 'Disabled'
   }
-  #Service FirewallDisable {
-  #  Name = 'WinDefend'
-  #  State = 'Stopped'
-  #  StartupType = 'Disabled'
-  #}
-  #Service PuppetDisable {
-  #  Name = 'puppet'
-  #  State = 'Stopped'
-  #  StartupType = 'Disabled'
-  #}
 }
--- userdata/try-win2012-vs2013/SoftwareConfig.ps1
+++ userdata/try-win2012-vs2013/SoftwareConfig.ps1
@@ -1,30 +1,30 @@
 # SoftwareConfig downloads and installs required software
 Configuration SoftwareConfig {
-  #Chocolatey SublimeText3Install {
-  #  Ensure = 'Present'
-  #  Package = 'sublimetext3'
-  #  Version = '3.0.0.3103'
-  #}
-  #Chocolatey SublimeText3PackageControlInstall {
-  #  Ensure = 'Present'
-  #  Package = 'sublimetext3.packagecontrol'
-  #  Version = '2.0.0.20140915'
-  #}
+  Chocolatey SublimeText3Install {
+    Ensure = 'Present'
+    Package = 'sublimetext3'
+    Version = '3.0.0.3103'
+  }
+  Chocolatey SublimeText3PackageControlInstall {
+    Ensure = 'Present'
+    Package = 'sublimetext3.packagecontrol'
+    Version = '2.0.0.20140915'
+  }
   #Chocolatey VisualStudioCommunity2013Install {
   #  Ensure = 'Present'
   #  Package = 'visualstudiocommunity2013'
   #  Version = '12.0.21005.1'
   #}
-  #Chocolatey WindowsSdkInstall {
-  #  Ensure = 'Present'
-  #  Package = 'windows-sdk-8.1'
-  #  Version = '8.100.26654.0'
-  #}
-  #Chocolatey RustInstall {
-  #  Ensure = 'Present'
-  #  Package = 'rust'
-  #  Version = '1.6.0'
-  #}
+  Chocolatey VisualStudio2015CommunityInstall {
+    Ensure = 'Present'
+    Package = 'visualstudio2015community'
+    Version = '14.0.24720.01'
+  }
+  Chocolatey WindowsSdkInstall {
+    Ensure = 'Present'
+    Package = 'windows-sdk-8.1'
+    Version = '8.100.26654.0'
+  }
   Import-DscResource -ModuleName PSDesiredStateConfiguration
   Package RustInstall {
     Name = 'Rust beta 1.7 (MSVC 64-bit)'
@@ -33,12 +33,12 @@ Configuration SoftwareConfig {
     Ensure = 'Present'
     LogPath = ('{0}\log\{1}.rust-beta-x86_64-pc-windows-msvc.msi.log' -f $env:SystemDrive, [DateTime]::Now.ToString("yyyyMMddHHmmss"))
   }
-  #Package MozillaBuildInstall {
-  #  Name = MozillaBuild
-  #  Path = 'http://ftp.mozilla.org/pub/mozilla/libraries/win32/MozillaBuildSetup-2.1.0.exe'
-  #  ProductId = ''
-  #  Arguments = '/S'
-  #  Ensure = Present
-  #  LogPath = ('{0}\log' -f $env:SystemDrive)
-  #}
+  Package MozillaBuildInstall {
+    Name = MozillaBuild
+    Path = 'http://ftp.mozilla.org/pub/mozilla/libraries/win32/MozillaBuildSetup-2.1.0.exe'
+    ProductId = ''
+    Arguments = '/S'
+    Ensure = 'Present'
+    LogPath = ('{0}\log\{1}.MozillaBuildSetup-2.1.0.exe.log' -f $env:SystemDrive, [DateTime]::Now.ToString("yyyyMMddHHmmss"))
+  }
 }
