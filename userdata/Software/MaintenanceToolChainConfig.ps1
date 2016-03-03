58d7d12769f50640ce72bdbd6f0e1aeeb1258254
Rob Thijssen <rthijssen@gmail.com>
2016-03-03 19:19:28 +0000

    testing

--- userdata/Software/MaintenanceToolChainConfig.ps1
+++ userdata/Software/MaintenanceToolChainConfig.ps1
@@ -12,28 +12,28 @@ Configuration MaintenanceToolChainConfig {
     Ensure = 'Present'
   }
 
-  #Chocolatey NxLogInstall {
-  #  Ensure = 'Present'
-  #  Package = 'nxlog'
-  #  Version = '2.9.1504'
-  #}
-
-
-  Script NxLogDownload {
-    GetScript = { @{ Result = (Test-Path -Path ('{0}\Temp\nxlog-ce-2.9.1504.msi' -f $env:SystemRoot) -ErrorAction SilentlyContinue) } }
-    SetScript = {
-      (New-Object Net.WebClient).DownloadFile('http://nxlog.org/system/files/products/files/1/nxlog-ce-2.9.1504.msi', ('{0}\Temp\nxlog-ce-2.9.1504.msi' -f $env:SystemRoot))
-      Unblock-File -Path ('{0}\Temp\nxlog-ce-2.9.1504.msi' -f $env:SystemRoot)
-    }
-    TestScript = { if (Test-Path -Path ('{0}\Temp\nxlog-ce-2.9.1504.msi' -f $env:SystemRoot) -ErrorAction SilentlyContinue) { $true } else { $false } }
-  }
-  Package NxLogInstall {
-    Name = 'NxLog'
-    Path = ('{0}\Temp\nxlog-ce-2.9.1504.msi' -f $env:SystemRoot)
-    ProductId = ''
+  Chocolatey NxLogInstall {
     Ensure = 'Present'
-    LogPath = ('{0}\log\{1}.nxlog-ce-2.9.1504.msi.log' -f $env:SystemDrive, [DateTime]::Now.ToString("yyyyMMddHHmmss"))
+    Package = 'nxlog'
+    Version = '2.9.1504'
   }
+
+
+  #Script NxLogDownload {
+  #  GetScript = { @{ Result = (Test-Path -Path ('{0}\Temp\nxlog-ce-2.9.1504.msi' -f $env:SystemRoot) -ErrorAction SilentlyContinue) } }
+  #  SetScript = {
+  #    (New-Object Net.WebClient).DownloadFile('http://nxlog.org/system/files/products/files/1/nxlog-ce-2.9.1504.msi', ('{0}\Temp\nxlog-ce-2.9.1504.msi' -f $env:SystemRoot))
+  #    Unblock-File -Path ('{0}\Temp\nxlog-ce-2.9.1504.msi' -f $env:SystemRoot)
+  #  }
+  #  TestScript = { if (Test-Path -Path ('{0}\Temp\nxlog-ce-2.9.1504.msi' -f $env:SystemRoot) -ErrorAction SilentlyContinue) { $true } else { $false } }
+  #}
+  #Package NxLogInstall {
+  #  Name = 'NxLog'
+  #  Path = ('{0}\Temp\nxlog-ce-2.9.1504.msi' -f $env:SystemRoot)
+  #  ProductId = ''
+  #  Ensure = 'Present'
+  #  LogPath = ('{0}\log\{1}.nxlog-ce-2.9.1504.msi.log' -f $env:SystemDrive, [DateTime]::Now.ToString("yyyyMMddHHmmss"))
+  #}
   Script NxLogConfigure {
     GetScript = { @{ Result = ((Test-Path -Path ('{0}\nxlog\cert\papertrail-bundle.pem' -f ${env:ProgramFiles(x86)}) -ErrorAction SilentlyContinue) -and (((Get-Content ('{0}\nxlog\conf\nxlog.conf' -f ${env:ProgramFiles(x86)})) | %{ $_ -match 'papertrail-bundle.pem' }) -contains $true) -and (Get-Service 'nxlog' -ErrorAction SilentlyContinue)) } }
     SetScript = {
