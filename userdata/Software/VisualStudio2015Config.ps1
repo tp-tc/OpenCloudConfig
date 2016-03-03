52a9f34189a4902962c38ed3d4304f964fc02496
Rob Thijssen <rthijssen@gmail.com>
2016-03-03 12:49:06 +0000

    testing

--- userdata/Software/VisualStudio2015Config.ps1
+++ userdata/Software/VisualStudio2015Config.ps1
@@ -18,7 +18,13 @@ Configuration VisualStudio2015Config {
   }
   Script VisualStudio2015SymbolicLink {
     GetScript = { @{ Result = (Test-Path -Path ('{0}\tools\vs2015' -f $env:SystemDrive) -ErrorAction SilentlyContinue) } }
-    SetScript = { New-Item -ItemType SymbolicLink -Path ('{0}\tools' -f $env:SystemDrive) -Name 'vs2015' -Target ('{0}\Microsoft Visual Studio 14.0' -f ${env:ProgramFiles(x86)}) }
+    SetScript = {
+      if ($PSVersionTable.PSVersion.Major -gt 4) {
+        New-Item -ItemType SymbolicLink -Path ('{0}\tools' -f $env:SystemDrive) -Name 'vs2015' -Target ('{0}\Microsoft Visual Studio 14.0' -f ${env:ProgramFiles(x86)})
+      } else {
+        & cmd @('/c', 'mklink', '/D', ('{0}\tools\vs2015' -f $env:SystemDrive), ('{0}\Microsoft Visual Studio 14.0' -f ${env:ProgramFiles(x86)}))
+      }
+    }
     TestScript = { (Test-Path -Path ('{0}\tools\vs2015' -f $env:SystemDrive)) }
   }
 }
