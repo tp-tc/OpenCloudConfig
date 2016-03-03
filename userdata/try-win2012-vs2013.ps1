03af0f46a95f7d4dfa9bcefd1c4ae9ed91a33491
Rob Thijssen <rthijssen@gmail.com>
2016-03-03 13:03:51 +0000

    testing

--- userdata/try-win2012-vs2013.ps1
+++ userdata/try-win2012-vs2013.ps1
@@ -11,16 +11,22 @@ function Run-RemoteDesiredStateConfig {
   Invoke-Expression "$config -OutputPath $mof"
   Start-DscConfiguration -Path "$mof" -Wait -Verbose -Force
 }
-$url = 'https://raw.githubusercontent.com/MozRelOps/OpenCloudConfig/master/userdata'
-$configs = @(
-  'FeatureConfig',
-  'ResourceConfig',
-  'Software/VisualStudio2013Config',
-  'Software/CompilerToolChainConfig',
-  'Software/TaskClusterToolChainConfig',
-  'Software/MaintenanceToolChainConfig',
-  'ServiceConfig'
-)
-foreach ($config in $configs) {
-  Run-RemoteDesiredStateConfig -url ('{0}/{1}.ps1' -f $url, $config)
+if ($PSVersionTable.PSVersion.Major -lt 4) {
+  Invoke-Expression ((New-Object Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+  & choco @('upgrade', 'powershell', '-y')
+  & shutdown @('-r', '-t', '0', '-c', 'Powershell upgraded', '-f', '-d', 'p:4:1')
+} else {
+  $url = 'https://raw.githubusercontent.com/MozRelOps/OpenCloudConfig/master/userdata'
+  $configs = @(
+    'FeatureConfig',
+    'ResourceConfig',
+    'Software/VisualStudio2013Config',
+    'Software/CompilerToolChainConfig',
+    'Software/TaskClusterToolChainConfig',
+    'Software/MaintenanceToolChainConfig',
+    'ServiceConfig'
+  )
+  foreach ($config in $configs) {
+    Run-RemoteDesiredStateConfig -url ('{0}/{1}.ps1' -f $url, $config)
+  }
 }
