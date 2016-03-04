f69b3ec193fdff4c99f63503500ed2d305228cef
Rob Thijssen <rthijssen@gmail.com>
2016-03-04 11:12:51 +0000

    testing

--- userdata/Software/MaintenanceToolChainConfig.ps1
+++ userdata/Software/MaintenanceToolChainConfig.ps1
@@ -12,28 +12,26 @@ Configuration MaintenanceToolChainConfig {
     Ensure = 'Present'
   }
 
-  Chocolatey NxLogInstall {
-    Ensure = 'Present'
-    Package = 'nxlog'
-    Version = '2.9.1504'
-  }
-
-
-  #Script NxLogDownload {
-  #  GetScript = { @{ Result = (Test-Path -Path ('{0}\Temp\nxlog-ce-2.9.1504.msi' -f $env:SystemRoot) -ErrorAction SilentlyContinue) } }
-  #  SetScript = {
-  #    (New-Object Net.WebClient).DownloadFile('http://nxlog.org/system/files/products/files/1/nxlog-ce-2.9.1504.msi', ('{0}\Temp\nxlog-ce-2.9.1504.msi' -f $env:SystemRoot))
-  #    Unblock-File -Path ('{0}\Temp\nxlog-ce-2.9.1504.msi' -f $env:SystemRoot)
-  #  }
-  #  TestScript = { if (Test-Path -Path ('{0}\Temp\nxlog-ce-2.9.1504.msi' -f $env:SystemRoot) -ErrorAction SilentlyContinue) { $true } else { $false } }
-  #}
-  #Package NxLogInstall {
-  #  Name = 'NxLog'
-  #  Path = ('{0}\Temp\nxlog-ce-2.9.1504.msi' -f $env:SystemRoot)
-  #  ProductId = ''
+  #Chocolatey NxLogInstall {
   #  Ensure = 'Present'
-  #  LogPath = ('{0}\log\{1}.nxlog-ce-2.9.1504.msi.log' -f $env:SystemDrive, [DateTime]::Now.ToString("yyyyMMddHHmmss"))
+  #  Package = 'nxlog'
+  #  Version = '2.9.1504'
   #}
+  Script NxLogDownload {
+    GetScript = { @{ Result = (Test-Path -Path ('{0}\Temp\nxlog-ce-2.9.1504.msi' -f $env:SystemRoot) -ErrorAction SilentlyContinue) } }
+    SetScript = {
+      (New-Object Net.WebClient).DownloadFile('http://nxlog.org/system/files/products/files/1/nxlog-ce-2.9.1504.msi', ('{0}\Temp\nxlog-ce-2.9.1504.msi' -f $env:SystemRoot))
+      Unblock-File -Path ('{0}\Temp\nxlog-ce-2.9.1504.msi' -f $env:SystemRoot)
+    }
+    TestScript = { if (Test-Path -Path ('{0}\Temp\nxlog-ce-2.9.1504.msi' -f $env:SystemRoot) -ErrorAction SilentlyContinue) { $true } else { $false } }
+  }
+  Package NxLogInstall {
+    Name = 'NxLog'
+    Path = ('{0}\Temp\nxlog-ce-2.9.1504.msi' -f $env:SystemRoot)
+    ProductId = ''
+    Ensure = 'Present'
+    LogPath = ('{0}\log\{1}.nxlog-ce-2.9.1504.msi.log' -f $env:SystemDrive, [DateTime]::Now.ToString("yyyyMMddHHmmss"))
+  }
   Script NxLogConfigure {
     GetScript = { @{ Result = ((Test-Path -Path ('{0}\nxlog\cert\papertrail-bundle.pem' -f ${env:ProgramFiles(x86)}) -ErrorAction SilentlyContinue) -and (((Get-Content ('{0}\nxlog\conf\nxlog.conf' -f ${env:ProgramFiles(x86)})) | %{ $_ -match 'papertrail-bundle.pem' }) -contains $true) -and (Get-Service 'nxlog' -ErrorAction SilentlyContinue)) } }
     SetScript = {
@@ -46,15 +44,30 @@ Configuration MaintenanceToolChainConfig {
     TestScript = { if ((Test-Path -Path ('{0}\nxlog\cert\papertrail-bundle.pem' -f ${env:ProgramFiles(x86)}) -ErrorAction SilentlyContinue) -and (((Get-Content ('{0}\nxlog\conf\nxlog.conf' -f ${env:ProgramFiles(x86)})) | %{ $_ -match 'papertrail-bundle.pem' }) -contains $true) -and (Get-Service 'nxlog' -ErrorAction SilentlyContinue)) { $true } else { $false } }
   }
 
-  Chocolatey SublimeText3Install {
-    Ensure = 'Present'
-    Package = 'sublimetext3'
-    Version = '3.0.0.3103'
+  #Chocolatey SublimeText3Install {
+  #  Ensure = 'Present'
+  #  Package = 'sublimetext3'
+  #  Version = '3.0.0.3103'
+  #}
+  #Chocolatey SublimeText3PackageControlInstall {
+  #  Ensure = 'Present'
+  #  Package = 'sublimetext3.packagecontrol'
+  #  Version = '2.0.0.20140915'
+  #}
+  Script SublimeText3Download {
+    GetScript = { @{ Result = (Test-Path -Path ('{0}\Temp\sublime-text-setup.exe' -f $env:SystemDrive) -ErrorAction SilentlyContinue) } }
+    SetScript = {
+      (New-Object Net.WebClient).DownloadFile(('https://download.sublimetext.com/{0}' -f @{$true='Sublime%20Text%20Build%203103%20x64%20Setup.exe';$false='Sublime%20Text%20Build%203103%20Setup.exe'}[(Test-Path Env:\'ProgramFiles(x86)')]), ('{0}\Temp\sublime-text-setup.exe' -f $env:SystemDrive))
+      Unblock-File -Path ('{0}\Temp\sublime-text-setup.exe' -f $env:SystemDrive)
+    }
+    TestScript = { if (Test-Path -Path ('{0}\Temp\sublime-text-setup.exe' -f $env:SystemDrive) -ErrorAction SilentlyContinue) { $true } else { $false } }
   }
-  Chocolatey SublimeText3PackageControlInstall {
-    Ensure = 'Present'
-    Package = 'sublimetext3.packagecontrol'
-    Version = '2.0.0.20140915'
+  Script SublimeText3Install {
+    GetScript = { @{ Result = (Test-Path -Path ('{0}\SublimeText\sublime_text.exe' -f $env:ProgramFiles) -ErrorAction SilentlyContinue) } }
+    SetScript = {
+      Start-Process ('{0}\Temp\sublime-text-setup.exe' -f $env:SystemDrive) -ArgumentList '/VERYSILENT /NORESTART /TASKS="contextentry"' -Wait -NoNewWindow -PassThru -RedirectStandardOutput ('{0}\log\{1}.sublime-text-setup.exe.stdout.log' -f $env:SystemDrive, [DateTime]::Now.ToString("yyyyMMddHHmmss")) -RedirectStandardError ('{0}\log\{1}.sublime-text-setup.exe.stderr.log' -f $env:SystemDrive, [DateTime]::Now.ToString("yyyyMMddHHmmss"))
+    }
+    TestScript = { (Test-Path -Path ('{0}\SublimeText\sublime_text.exe' -f $env:ProgramFiles) -ErrorAction SilentlyContinue) }
   }
 
   Script CygWinDownload {
@@ -70,7 +83,7 @@ Configuration MaintenanceToolChainConfig {
     SetScript = {
       Start-Process ('{0}\cygwin-setup-x86_64.exe' -f $env:Temp) -ArgumentList ('--quiet-mode --wait --root {0}\cygwin --site http://cygwin.mirror.constant.com --packages openssh,vim,curl,tar,wget,zip,unzip,diffutils,bzr' -f $env:SystemDrive) -Wait -NoNewWindow -PassThru -RedirectStandardOutput ('{0}\log\{1}.MozillaBuildSetup-2.1.0.exe.stdout.log' -f $env:SystemDrive, [DateTime]::Now.ToString("yyyyMMddHHmmss")) -RedirectStandardError ('{0}\log\{1}.MozillaBuildSetup-2.1.0.exe.stderr.log' -f $env:SystemDrive, [DateTime]::Now.ToString("yyyyMMddHHmmss"))
     }
-    TestScript = { (Test-Path -Path ('{0}\cygwin\bin\cygrunsrv.exe' -f $env:SystemDrive)) }
+    TestScript = { (Test-Path -Path ('{0}\cygwin\bin\cygrunsrv.exe' -f $env:SystemDrive) -ErrorAction SilentlyContinue) }
   }
   Script SshInboundFirewallEnable {
     GetScript = { @{ Result = (Get-NetFirewallRule -DisplayName 'Allow SSH inbound' -ErrorAction SilentlyContinue) } }
