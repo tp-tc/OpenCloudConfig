ebd7e800cc19dcb6e14626804324d9b5397f48fd
Rob Thijssen <rthijssen@gmail.com>
2016-03-09 15:07:51 -0800

    try another smtp construct

--- userdata/try-win2012-vs2013.ps1
+++ userdata/try-win2012-vs2013.ps1
@@ -18,13 +18,20 @@ function Send-Log {
     [string[]] $attachments = $null,
     [string] $to = 'releng-puppet-mail@mozilla.com',
     [string] $from = ('{0}@{1}.{2}' -f $env:USERNAME, $env:COMPUTERNAME, $env:USERDOMAIN),
-    [string] $smtpServer = 'email-smtp.us-east-1.amazonaws.com'
+    [string] $smtpServer = 'email-smtp.us-east-1.amazonaws.com',
+    [int] $smtpPort = 587
   )
   if (Test-Path $logfile) {
     (New-Object Net.WebClient).DownloadFile('https://github.com/MozRelOps/OpenCloudConfig/blob/master/userdata/Configuration/smtp.pass.gpg?raw=true', ('{0}\smtp.pass.gpg' -f $env:Temp))
     $password = (& ('{0}\GNU\GnuPG\pub\gpg.exe' -f ${env:ProgramFiles(x86)}) @('-u', 'Administrator', '-d', ('{0}\smtp.pass.gpg' -f $env:Temp)))
-    $credential = New-Object System.Management.Automation.PSCredential 'AKIAIPJEOD57YDLBF35Q', (ConvertTo-SecureString $password -AsPlainText -Force)
-    Send-MailMessage -To $to -Subject $subject -Body ([IO.File]::ReadAllText($logfile)) -SmtpServer $smtpServer -From $from -Attachments $attachments -Credential $credential -UseSsl
+
+    $smtp = New-Object Net.Mail.SmtpClient($smtpServer, $smtpPort)
+    $smtp.EnableSsl = $true
+    $smtp.Credentials = New-Object Net.NetworkCredential('AKIAIPJEOD57YDLBF35Q', $password)
+    $msg = New-Object Net.Mail.MailMessage($from, $to, $subject, ([IO.File]::ReadAllText($logfile)))
+    $smtp.Send($msg)
+    #$credential = New-Object System.Management.Automation.PSCredential 'AKIAIPJEOD57YDLBF35Q', (ConvertTo-SecureString $password -AsPlainText -Force)
+    #Send-MailMessage -To $to -Subject $subject -Body ([IO.File]::ReadAllText($logfile)) -SmtpServer $smtpServer -From $from -Attachments $attachments -Credential $credential -UseSsl
   } else {
     Write-Log -message ("{0} :: skipping log mail, file: {1} not found" -f $($MyInvocation.MyCommand.Name), $logfile) -severity 'WARN'
   }
