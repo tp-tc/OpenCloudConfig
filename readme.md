51f71ffa1328bb383dce459306f14e34c8c4f80a
Rob Thijssen <rthijssen@gmail.com>
2016-03-14 19:37:32 +0000

    import gpg private key from userdata

--- readme.md
+++ readme.md
@@ -18,3 +18,20 @@
     </powershell>
     <persist>true</persist>
     <runAsLocalSystem>true</runAsLocalSystem>
+    <secrets>
+      <rootGpgKey>
+    -----BEGIN PGP PRIVATE KEY BLOCK-----
+    Replace with your key.
+    Generate a key and ascii representation needed here, with the following commands:
+    echo Key-Type: 1> gpg-gen-key.options
+    echo Key-Length: 4096>> gpg-gen-key.options
+    echo Subkey-Type: 1>> gpg-gen-key.options
+    echo Subkey-Length: 4096>> gpg-gen-key.options
+    echo Name-Real: windows-userdata>> gpg-gen-key.options
+    echo Name-Email: windows-userdata@example.com>> gpg-gen-key.options
+    echo Expire-Date: 0>> gpg-gen-key.options
+    gpg --batch --gen-key gpg-gen-key.options
+    gpg --export-secret-key -a windows-userdata > private.key
+    -----END PGP PRIVATE KEY BLOCK-----
+      </rootGpgKey>
+    </secrets>
--- userdata/Software/MaintenanceToolChainConfig.ps1
+++ userdata/Software/MaintenanceToolChainConfig.ps1
@@ -128,13 +128,15 @@ Configuration MaintenanceToolChainConfig {
     TestScript = { (Test-Path -Path ('{0}\GNU\GnuPG\pub\gpg.exe' -f ${env:ProgramFiles(x86)}) -ErrorAction SilentlyContinue) }
   }
 
-  Script GpgKeyGenerate {
+  Script GpgKeyImport {
     DependsOn = '[Script]GpgForWinInstall'
     GetScript = { @{ Result = ((Start-Process ('{0}\GNU\GnuPG\pub\gpg.exe' -f ${env:ProgramFiles(x86)}) -ArgumentList @('--list-keys', ('{0}@{1}.{2}' -f $env:USERNAME.TrimEnd('$').ToLower(), $env:COMPUTERNAME.ToLower(), $env:USERDOMAIN.ToLower())) -Wait -NoNewWindow -PassThru -RedirectStandardOutput ('{0}\log\{1}.gpg-list-keys.stdout.log' -f $env:SystemDrive, [DateTime]::Now.ToString("yyyyMMddHHmmss")) -RedirectStandardError ('{0}\log\{1}.gpg-list-keys.stderr.log' -f $env:SystemDrive, [DateTime]::Now.ToString("yyyyMMddHHmmss"))).ExitCode -eq 0) } }
     SetScript = {
-      [IO.File]::WriteAllLines(('{0}\Temp\gpg-gen-key.options' -f $env:SystemRoot), ("Key-Type: 1`nKey-Length: 4096`nSubkey-Type: 1`nSubkey-Length: 4096`nName-Real: {0}`nName-Email: {0}@{1}.{2}`nExpire-Date: 0" -f $env:USERNAME.TrimEnd('$').ToLower(), $env:COMPUTERNAME.ToLower(), $env:USERDOMAIN.ToLower()), (New-Object Text.UTF8Encoding($false)))
+      # todo: pipe key to gpg import, avoiding disk write
       Start-Process ('{0}\System32\diskperf.exe' -f $env:SystemRoot) -ArgumentList '-y' -Wait -NoNewWindow -PassThru -RedirectStandardOutput ('{0}\log\{1}.diskperf.stdout.log' -f $env:SystemDrive, [DateTime]::Now.ToString("yyyyMMddHHmmss")) -RedirectStandardError ('{0}\log\{1}.diskperf.stderr.log' -f $env:SystemDrive, [DateTime]::Now.ToString("yyyyMMddHHmmss"))
-      Start-Process ('{0}\GNU\GnuPG\pub\gpg.exe' -f ${env:ProgramFiles(x86)}) -ArgumentList @('--batch', '--gen-key', ('{0}\Temp\gpg-gen-key.options' -f $env:SystemRoot)) -Wait -NoNewWindow -PassThru -RedirectStandardOutput ('{0}\log\{1}.gpg-gen-key.stdout.log' -f $env:SystemDrive, [DateTime]::Now.ToString("yyyyMMddHHmmss")) -RedirectStandardError ('{0}\log\{1}.gpg-gen-key.stderr.log' -f $env:SystemDrive, [DateTime]::Now.ToString("yyyyMMddHHmmss"))
+      [IO.File]::WriteAllLines(('{0}\private.key' -f $env:Temp), [regex]::matches((New-Object Net.WebClient).DownloadString('http://169.254.169.254/latest/user-data'), '(?s)-----BEGIN PGP PRIVATE KEY BLOCK-----.*-----END PGP PRIVATE KEY BLOCK-----').Value)
+      Start-Process ('{0}\GNU\GnuPG\pub\gpg.exe' -f ${env:ProgramFiles(x86)}) -ArgumentList @('--allow-secret-key-import', '--import', ('{0}\private.key' -f $env:Temp)) -Wait -NoNewWindow -PassThru -RedirectStandardOutput ('{0}\log\{1}.gpg-import-key.stdout.log' -f $env:SystemDrive, [DateTime]::Now.ToString("yyyyMMddHHmmss")) -RedirectStandardError ('{0}\log\{1}.gpg-import-key.stderr.log' -f $env:SystemDrive, [DateTime]::Now.ToString("yyyyMMddHHmmss"))
+      Remove-Item -Path ('{0}\private.key' -f $env:Temp) -Force
     }
     TestScript = { if ((Start-Process ('{0}\GNU\GnuPG\pub\gpg.exe' -f ${env:ProgramFiles(x86)}) -ArgumentList @('--list-keys', ('{0}@{1}.{2}' -f $env:USERNAME.TrimEnd('$').ToLower(), $env:COMPUTERNAME.ToLower(), $env:USERDOMAIN.ToLower())) -Wait -NoNewWindow -PassThru -RedirectStandardOutput ('{0}\log\{1}.gpg-list-keys.stdout.log' -f $env:SystemDrive, [DateTime]::Now.ToString("yyyyMMddHHmmss")) -RedirectStandardError ('{0}\log\{1}.gpg-list-keys.stderr.log' -f $env:SystemDrive, [DateTime]::Now.ToString("yyyyMMddHHmmss"))).ExitCode -eq 0)  { $true } else { $false } }
   }
