3bddd10d12f7e4e45340a96c6fbeff1733c619f6
Rob Thijssen <rthijssen@gmail.com>
2016-03-15 14:57:47 +0000

    create root user home

--- userdata/UserConfig.ps1
+++ userdata/UserConfig.ps1
@@ -1,10 +1,17 @@
 
 Configuration UserConfig {
   Import-DscResource -ModuleName PSDesiredStateConfiguration
+  File RootHome {
+    Type = 'Directory'
+    DestinationPath = ('{0}\Users\root' -f $env:SystemDrive)
+    Ensure = 'Present'
+  }
   Script RootUserCreate {
+    DependsOn = '[File]RootHome'
     GetScript = { @{ Result = (Get-WMiObject -class Win32_UserAccount | Where { $_.Name -eq 'root' }) } }
     SetScript = {
-      & net @('user', 'root', [Guid]::NewGuid().ToString().Substring(0, 13), '/ADD', '/active:yes')
+      & net @('user', 'root', [Guid]::NewGuid().ToString().Substring(0, 13), '/ADD', '/active:yes', '/expires:never')
+      #& icacls @(('{0}\Users\root' -f $env:SystemDrive), '/T', '/C', '/grant', 'Administrators:(F)')
     }
     TestScript = { if (Get-WMiObject -class Win32_UserAccount | Where { $_.Name -eq 'root' }) { $true } else { $false } }
   }
