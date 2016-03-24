b6ca244ec79264d57b14f0127b3c146921ebf877
Rob Thijssen <rthijssen@gmail.com>
2016-03-24 13:24:07 +0000

    added default console settings

--- userdata/RegistryConfig.ps1
+++ userdata/RegistryConfig.ps1
@@ -5,66 +5,74 @@ Configuration RegistryConfig {
   Registry ConsoleDefaultQuickEdit {
     Ensure = 'Present'
     Force = $true
+    Hex = $true
     Key = 'HKEY_USERS\.DEFAULT\Console'
     ValueName = 'QuickEdit'
     ValueType = 'Dword'
-    ValueData = 0x00000001 # on
+    ValueData = '0x00000001' # on
   }
   Registry ConsoleDefaultInsertMode {
     Ensure = 'Present'
     Force = $true
+    Hex = $true
     Key = 'HKEY_USERS\.DEFAULT\Console'
     ValueName = 'InsertMode'
     ValueType = 'Dword'
-    ValueData = 0x00000001 # on
+    ValueData = '0x00000001' # on
   }
   Registry ConsoleDefaultScreenBufferSize {
     Ensure = 'Present'
     Force = $true
+    Hex = $true
     Key = 'HKEY_USERS\.DEFAULT\Console'
     ValueName = 'ScreenBufferSize'
     ValueType = 'Dword'
-    ValueData = 0x012c00a0 # 160x300
+    ValueData = '0x012c00a0' # 160x300
   }
   Registry ConsoleDefaultWindowSize {
     Ensure = 'Present'
     Force = $true
+    Hex = $true
     Key = 'HKEY_USERS\.DEFAULT\Console'
     ValueName = 'WindowSize'
     ValueType = 'Dword'
-    ValueData = 0x003c00a0 # 160x60
+    ValueData = '0x003c00a0' # 160x60
   }
   Registry ConsoleDefaultHistoryBufferSize {
     Ensure = 'Present'
     Force = $true
+    Hex = $true
     Key = 'HKEY_USERS\.DEFAULT\Console'
     ValueName = 'HistoryBufferSize'
     ValueType = 'Dword'
-    ValueData = 0x000003e7 # 999 (max)
+    ValueData = '0x000003e7' # 999 (max)
   }
   Registry ConsoleDefaultScreenColors {
     Ensure = 'Present'
     Force = $true
+    Hex = $true
     Key = 'HKEY_USERS\.DEFAULT\Console'
     ValueName = 'ScreenColors'
     ValueType = 'Dword'
-    ValueData = 0x0000000a # green on black
+    ValueData = '0x0000000a' # green on black
   }
   Registry ConsoleDefaultFontSize {
     Ensure = 'Present'
     Force = $true
+    Hex = $true
     Key = 'HKEY_USERS\.DEFAULT\Console'
     ValueName = 'FontSize'
     ValueType = 'Dword'
-    ValueData = 0x000c0000 # 12
+    ValueData = '0x000c0000' # 12
   }
   Registry ConsoleDefaultFontFamily {
     Ensure = 'Present'
     Force = $true
+    Hex = $true
     Key = 'HKEY_USERS\.DEFAULT\Console'
     ValueName = 'FontFamily'
     ValueType = 'Dword'
-    ValueData = 0x00000036 # Consolas
+    ValueData = '0x00000036' # Consolas
   }
   Registry ConsoleDefaultFaceName {
     Ensure = 'Present'
