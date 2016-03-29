d5030d547b1a49b0640e96402f67914179f064f7
Rob Thijssen <rthijssen@gmail.com>
2016-03-29 15:39:19 +0100

    synced 2015 userdata script

--- userdata/try-win2012-vs2015.ps1
+++ userdata/try-win2012-vs2015.ps1
@@ -11,23 +11,30 @@ function Run-RemoteDesiredStateConfig {
   Invoke-Expression "$config -OutputPath $mof"
   Start-DscConfiguration -Path "$mof" -Wait -Verbose -Force
 }
-Set-ExecutionPolicy RemoteSigned -force
+$logFile = ('{0}\log\{1}.userdata-run.log' -f $env:SystemDrive, [DateTime]::Now.ToString("yyyyMMddHHmmss"))
+New-Item -ItemType Directory -Force -Path ('{0}\log' -f $env:SystemDrive)
+Set-ExecutionPolicy RemoteSigned -force | Tee-Object -filePath $logFile -append
 if ($PSVersionTable.PSVersion.Major -lt 4) {
-  Invoke-Expression ((New-Object Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-  & choco @('upgrade', 'powershell', '-y')
-  & shutdown @('-r', '-t', '0', '-c', 'Powershell upgraded', '-f', '-d', 'p:4:1')
+  Invoke-Expression ((New-Object Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')) | Tee-Object -filePath $logFile -append
+  & choco @('upgrade', 'powershell', '-y') | Out-File -filePath $logFile -append
+  & shutdown @('-r', '-t', '0', '-c', 'Powershell upgraded', '-f', '-d', 'p:4:1') | Out-File -filePath $logFile -append
 } else {
   $url = 'https://raw.githubusercontent.com/MozRelOps/OpenCloudConfig/master/userdata'
   $configs = @(
-    'FeatureConfig',
     'ResourceConfig',
+    'UserConfig',
+    'Software/MaintenanceToolChainConfig',
     'Software/VisualStudio2015Config',
+    'FeatureConfig',
     'Software/CompilerToolChainConfig',
     'Software/TaskClusterToolChainConfig',
-    'Software/MaintenanceToolChainConfig',
-    'ServiceConfig'
+    'ServiceConfig',
+    'RegistryConfig'
   )
+  Start-Transcript -Path $logFile -Append
   foreach ($config in $configs) {
     Run-RemoteDesiredStateConfig -url ('{0}/{1}.ps1' -f $url, $config)
   }
+  Stop-Transcript
+  Run-RemoteDesiredStateConfig -url ('{0}/MaintenanceConfig.ps1' -f $url)
 }
