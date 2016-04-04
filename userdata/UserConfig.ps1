ced2d3b996ca6fa6e0745b713a316a593a0b2636
Rob Thijssen <rthijssen@gmail.com>
2016-04-04 15:16:25 +0100

    dont create root home

--- userdata/UserConfig.ps1
+++ userdata/UserConfig.ps1
@@ -1,13 +1,7 @@
 
 Configuration UserConfig {
   Import-DscResource -ModuleName PSDesiredStateConfiguration
-  File RootHome {
-    Type = 'Directory'
-    DestinationPath = ('{0}\Users\root' -f $env:SystemDrive)
-    Ensure = 'Present'
-  }
   Script RootUserCreate {
-    DependsOn = '[File]RootHome'
     GetScript = { @{ Result = (Get-WMiObject -class Win32_UserAccount | Where { $_.Name -eq 'root' }) } }
     SetScript = {
       $password = [regex]::matches((New-Object Net.WebClient).DownloadString('http://169.254.169.254/latest/user-data'), '(?s)<rootPassword>(.*)</rootPassword>').Groups[1].Value
