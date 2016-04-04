23ce6a029f019c8c658a287a389c3ffbd3c0598e
Rob Thijssen <rthijssen@gmail.com>
2016-04-04 15:01:21 +0100

    use root password from userdata

--- userdata/UserConfig.ps1
+++ userdata/UserConfig.ps1
@@ -10,7 +10,7 @@ Configuration UserConfig {
     DependsOn = '[File]RootHome'
     GetScript = { @{ Result = (Get-WMiObject -class Win32_UserAccount | Where { $_.Name -eq 'root' }) } }
     SetScript = {
-      & net @('user', 'root', [Guid]::NewGuid().ToString().Substring(0, 13), '/ADD', '/active:yes', '/expires:never')
+      & net @('user', 'root', [regex]::matches((New-Object Net.WebClient).DownloadString('http://169.254.169.254/latest/user-data'), '(?s)<rootPassword>(.*)</rootPassword>').Groups[1].Value, '/ADD', '/active:yes', '/expires:never')
       #& icacls @(('{0}\Users\root' -f $env:SystemDrive), '/T', '/C', '/grant', 'Administrators:(F)')
     }
     TestScript = { if (Get-WMiObject -class Win32_UserAccount | Where { $_.Name -eq 'root' }) { $true } else { $false } }
