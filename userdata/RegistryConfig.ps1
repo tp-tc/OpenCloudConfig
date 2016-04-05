9086ecd2979507cd00cdd289131467c242000b91
Rob Thijssen <rthijssen@gmail.com>
2016-04-05 15:40:11 +0100

    registry tweaks

--- userdata/RegistryConfig.ps1
+++ userdata/RegistryConfig.ps1
@@ -2,7 +2,6 @@
 # RegistryConfig sets or unsets registry values
 Configuration RegistryConfig {
   Import-DscResource -ModuleName PSDesiredStateConfiguration
-
   # https://bugzilla.mozilla.org/show_bug.cgi?id=1261812
   Registry WindowsErrorReportingLocalDumps {
     Ensure = 'Present'
@@ -18,26 +17,5 @@ Configuration RegistryConfig {
     ValueName = 'DontShowUI'
     ValueType = 'Dword'
     ValueData = '0x00000001' # on
-  }
-
-  # show file extensions by default
-  Registry ExplorerFolderHideFileExtOff {
-    Ensure = 'Present'
-    Force = $true
-    Hex = $true
-    Key = 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\Folder\HideFileExt'
-    ValueName = 'DefaultValue'
-    ValueType = 'Dword'
-    ValueData = '0'
-  }
-  # show full paths by default
-  Registry ExplorerFolderShowFullPathOn {
-    Ensure = 'Present'
-    Force = $true
-    Hex = $true
-    Key = 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\Folder\ShowFullPath'
-    ValueName = 'DefaultValue'
-    ValueType = 'Dword'
-    ValueData = '0x00000001'
   }
 }
