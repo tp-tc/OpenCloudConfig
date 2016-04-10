96b39f4b13e0b4b3661201d6b0fd84d0aa8bbacd
Rob Thijssen <rthijssen@gmail.com>
2016-04-10 17:14:37 +0100

    path settings

--- userdata/EnvironmentConfig.ps1
+++ userdata/EnvironmentConfig.ps1
@@ -6,7 +6,6 @@ file, You can obtain one at http://mozilla.org/MPL/2.0/.
 Configuration EnvironmentConfig {
   Import-DscResource -ModuleName PSDesiredStateConfiguration
   Script Path {
-    DependsOn = @('[Script]MozillaBuildInstall')
     GetScript = { @{ Result = $false } }
     SetScript = {
       $env:PATH = (((($env:PATH -split ';') + @(
