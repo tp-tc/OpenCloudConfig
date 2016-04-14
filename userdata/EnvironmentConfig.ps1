c664fd40972516e0ed24aacfcddb1ca21c6e0aab
Rob Thijssen <rthijssen@gmail.com>
2016-04-14 14:44:03 +0100

    win 2008

--- userdata/EnvironmentConfig.ps1
+++ userdata/EnvironmentConfig.ps1
@@ -8,12 +8,19 @@ Configuration EnvironmentConfig {
   Script Path {
     GetScript = { @{ Result = $false } }
     SetScript = {
+      if (Test-Path -Path ('{0}\mozilla-build\python' -f $env:SystemDrive) -ErrorAction SilentlyContinue) {
+        $pythonPath = ('{0}\mozilla-build\python' -f $env:SystemDrive)
+      } elseif (Test-Path -Path ('{0}\mozilla-build\python27' -f $env:SystemDrive) -ErrorAction SilentlyContinue) {
+        $pythonPath = ('{0}\mozilla-build\python27' -f $env:SystemDrive)
+      } else {
+        $pythonPath = ('{0}\Python27' -f $env:SystemDrive)
+      }
       $env:PATH = (((($env:PATH -split ';') + @(
         ('{0}\mozilla-build\info-zip' -f $env:SystemDrive),
         ('{0}\mozilla-build\msys\bin' -f $env:SystemDrive),
         ('{0}\mozilla-build\msys\local\bin' -f $env:SystemDrive),
-        ('{0}\mozilla-build\python' -f $env:SystemDrive),
-        ('{0}\mozilla-build\python\Scripts' -f $env:SystemDrive),
+        ('{0}' -f $pythonPath),
+        ('{0}\Scripts' -f $pythonPath),
         ('{0}\mozilla-build\yasm' -f $env:SystemDrive))) | select -Unique) -join ';')
       [Environment]::SetEnvironmentVariable('PATH', $env:PATH, 'Machine')
     }
--- userdata/win2008.ps1
+++ userdata/win2008.ps1
@@ -14,35 +14,42 @@ function Run-RemoteDesiredStateConfig {
 $logFile = ('{0}\log\{1}.userdata-run.log' -f $env:SystemDrive, [DateTime]::Now.ToString("yyyyMMddHHmmss"))
 New-Item -ItemType Directory -Force -Path ('{0}\log' -f $env:SystemDrive)
 Set-ExecutionPolicy RemoteSigned -force | Tee-Object -filePath $logFile -append
-if ($PSVersionTable.PSVersion.Major -lt 4) {
-  Get-Service wuauserv | Set-Service -StartupType manual
-  Get-Service wuauserv | Start-Service -PassThru
-  Invoke-Expression ((New-Object Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')) | Tee-Object -filePath $logFile -append
-  & choco @('upgrade', 'powershell', '-y') | Out-File -filePath $logFile -append
-  & shutdown @('-r', '-t', '0', '-c', 'Powershell upgraded', '-f', '-d', 'p:4:1') | Out-File -filePath $logFile -append
+
+if (-not ([Net.Dns]::GetHostName() -ieq (New-Object Net.WebClient).DownloadString('http://169.254.169.254/latest/meta-data/instance-id'))) {
+  $env:COMPUTERNAME = (New-Object Net.WebClient).DownloadString('http://169.254.169.254/latest/meta-data/instance-id')
+  [Environment]::SetEnvironmentVariable("COMPUTERNAME", $env:COMPUTERNAME, "Machine")
+  (Get-WmiObject Win32_ComputerSystem).Rename($env:COMPUTERNAME)
 } else {
-  $url = 'https://raw.githubusercontent.com/MozRelOps/OpenCloudConfig/master/userdata'
-  $configs = @(
-    'ImportCloudToolsAmiConfig',
-    'UserConfig',
-    'Software/MaintenanceToolChainConfig',
-    'Software/TaskClusterToolChainConfig',
-    'FirefoxBuildResourcesConfig',
-    'ServiceConfig',
-    'RegistryConfig',
-    'EnvironmentConfig'
-  )
-  Start-Transcript -Path $logFile -Append
-  foreach ($config in $configs) {
-    Run-RemoteDesiredStateConfig -url ('{0}/{1}.ps1' -f $url, $config)
-  }
-  Stop-Transcript
-  if (((Get-Content $logFile) | % { $_ -match 'A reboot is required to progress further' }) -contains $true) {
-    & shutdown @('-r', '-t', '0', '-c', 'Userdata reboot required', '-f', '-d', 'p:4:1')
+  if ($PSVersionTable.PSVersion.Major -lt 4) {
+    Get-Service wuauserv | Set-Service -StartupType manual
+    Get-Service wuauserv | Start-Service -PassThru
+    Invoke-Expression ((New-Object Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')) | Tee-Object -filePath $logFile -append
+    & choco @('upgrade', 'powershell', '-y') | Out-File -filePath $logFile -append
+    & shutdown @('-r', '-t', '0', '-c', 'Powershell upgraded', '-f', '-d', 'p:4:1') | Out-File -filePath $logFile -append
   } else {
-    Run-RemoteDesiredStateConfig -url ('{0}/MaintenanceConfig.ps1' -f $url)
-    if ((Get-ChildItem -Path ('{0}\log' -f $env:SystemDrive) | Where-Object { $_.Name.EndsWith('.userdata-run.zip') }).Count -eq 1) {
-      & shutdown @('-s', '-t', '0', '-c', 'Userdata run complete', '-f', '-d', 'p:4:1')
+    $url = 'https://raw.githubusercontent.com/MozRelOps/OpenCloudConfig/master/userdata'
+    $configs = @(
+      'ImportCloudToolsAmiConfig',
+      'UserConfig',
+      'Software/MaintenanceToolChainConfig',
+      'Software/TaskClusterToolChainConfig',
+      'FirefoxBuildResourcesConfig',
+      'ServiceConfig',
+      'RegistryConfig',
+      'EnvironmentConfig'
+    )
+    Start-Transcript -Path $logFile -Append
+    foreach ($config in $configs) {
+      Run-RemoteDesiredStateConfig -url ('{0}/{1}.ps1' -f $url, $config)
+    }
+    Stop-Transcript
+    if (((Get-Content $logFile) | % { $_ -match 'A reboot is required to progress further' }) -contains $true) {
+      & shutdown @('-r', '-t', '0', '-c', 'Userdata reboot required', '-f', '-d', 'p:4:1')
+    } else {
+      Run-RemoteDesiredStateConfig -url ('{0}/MaintenanceConfig.ps1' -f $url)
+      if ((Get-ChildItem -Path ('{0}\log' -f $env:SystemDrive) | Where-Object { $_.Name.EndsWith('.userdata-run.zip') }).Count -eq 1) {
+        & shutdown @('-s', '-t', '0', '-c', 'Userdata run complete', '-f', '-d', 'p:4:1')
+      }
     }
   }
 }
