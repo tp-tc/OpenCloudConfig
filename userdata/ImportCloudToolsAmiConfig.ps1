374f2b1a073a86228becd70980d3f892044a471b
Rob Thijssen <rthijssen@gmail.com>
2016-04-14 13:26:45 +0100

    win 2008

--- userdata/ImportCloudToolsAmiConfig.ps1
+++ userdata/ImportCloudToolsAmiConfig.ps1
@@ -4,6 +4,7 @@ Configuration ImportCloudToolsAmiConfig {
   Script CltbldUserRemove {
     GetScript = { @{ Result = (-not (Get-WMiObject -class Win32_UserAccount | Where { $_.Name -eq 'root' })) } }
     SetScript = {
+      Start-Process 'logoff' -ArgumentList @((((quser /server:. | ? { $_ -match 'cltbld' }) -split ' +')[2]), '/server:.') -Wait -NoNewWindow -PassThru -RedirectStandardOutput ('{0}\log\{1}.net-user-cltbld-logoff.stdout.log' -f $env:SystemDrive, [DateTime]::Now.ToString("yyyyMMddHHmmss")) -RedirectStandardError ('{0}\log\{1}.net-user-cltbld-logoff.stderr.log' -f $env:SystemDrive, [DateTime]::Now.ToString("yyyyMMddHHmmss"))
       Start-Process 'net' -ArgumentList @('user', 'cltbld', '/DELETE') -Wait -NoNewWindow -PassThru -RedirectStandardOutput ('{0}\log\{1}.net-user-cltbld-delete.stdout.log' -f $env:SystemDrive, [DateTime]::Now.ToString("yyyyMMddHHmmss")) -RedirectStandardError ('{0}\log\{1}.net-user-cltbld-delete.stderr.log' -f $env:SystemDrive, [DateTime]::Now.ToString("yyyyMMddHHmmss"))
     }
     TestScript = { if (-not (Get-WMiObject -class Win32_UserAccount | Where { $_.Name -eq 'root' })) { $true } else { $false } }
@@ -23,6 +24,21 @@ Configuration ImportCloudToolsAmiConfig {
       GetScript = { @{ Result = (-not (Test-Path -Path $using:path -ErrorAction SilentlyContinue)) } }
       SetScript = { Remove-Item $using:path -Confirm:$false -force }
       TestScript = { if (-not (Test-Path -Path $using:path -ErrorAction SilentlyContinue)) { $true } else { $false } }
+    }
+  }
+  $services = @(
+    'KTS',
+    'uvnc_service'
+  )
+  foreach ($service in $services) {
+    Script ('ServiceRemove-{0}' -f $service.Replace(' ', '_')) {
+      GetScript = { @{ Result = (-not (Get-Service -Name $using:service -ErrorAction SilentlyContinue)) } }
+      SetScript = {
+        Get-Service -Name $using:service | Stop-Service -PassThru
+        $service = Get-WmiObject -Class Win32_Service -Filter "Name='$using:service'"
+        $service.delete()
+      }
+      TestScript = { if (-not (Get-Service -Name $using:service -ErrorAction SilentlyContinue)) { $true } else { $false } }
     }
   }
 }
