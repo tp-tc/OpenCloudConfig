cfad91e116334f831acb83fdbd2a28d12862df54
Rob Thijssen <rthijssen@gmail.com>
2016-04-15 14:08:38 +0100

    json manifests

--- userdata/DynamicConfig.ps1
+++ userdata/DynamicConfig.ps1
@@ -84,9 +84,9 @@ Configuration DynamicConfig {
               # if no validations are specified, this function will return $false and cause the exe package to be (re)installed.
               (
                 ($using:item.Validate) -and (
-                  ($using:item.Validate.PathsExist -and ($using:item.Validate.Paths.Length -gt 0))
-                  -or ($using:item.Validate.CommandsReturn -and ($using:item.Validate.CommandsReturn.Length -gt 0))
-                  -or ($using:item.Validate.FilesContain -and ($using:item.Validate.FilesContain.Length -gt 0))
+                  (($using:item.Validate.PathsExist) -and ($using:item.Validate.Paths.Length -gt 0))
+                  -or (($using:item.Validate.CommandsReturn) -and ($using:item.Validate.CommandsReturn.Length -gt 0))
+                  -or (($using:item.Validate.FilesContain) -and ($using:item.Validate.FilesContain.Length -gt 0))
                 )
               )
 
@@ -96,7 +96,7 @@ Configuration DynamicConfig {
 
                 -or (
                   # validation paths-exist are specified
-                  ($using:item.Validate.PathsExist -and ($using:item.Validate.PathsExist.Length -gt 0))
+                  (($using:item.Validate.PathsExist) -and ($using:item.Validate.PathsExist.Length -gt 0))
 
                   # all validation paths-exist are satisfied (exist on the instance)
                   -and (-not (@($using:item.Validate.PathsExist | % {
@@ -111,7 +111,7 @@ Configuration DynamicConfig {
 
                 -or (
                   # validation commands-return are specified
-                  ($using:item.Validate.CommandsReturn -and ($using:item.Validate.CommandsReturn.Length -gt 0))
+                  (($using:item.Validate.CommandsReturn) -and ($using:item.Validate.CommandsReturn.Length -gt 0))
 
                   # all validation commands-return are satisfied
                   -and ($false) # todo: implement
@@ -124,7 +124,7 @@ Configuration DynamicConfig {
 
                 -or (
                   # validation files-contain are specified
-                  ($using:item.Validate.FilesContain -and ($using:item.Validate.FilesContain.Length -gt 0))
+                  (($using:item.Validate.FilesContain) -and ($using:item.Validate.FilesContain.Length -gt 0))
 
                   # all validation files-contain are satisfied
                   -and (-not (@($using:item.Validate.FilesContain | % {
