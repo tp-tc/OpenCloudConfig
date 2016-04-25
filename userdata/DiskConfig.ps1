78c75cfd91e75f452980dff6844d01a5227f08d6
Rob Thijssen <rthijssen@gmail.com>
2016-04-25 15:29:44 +0100

    spot disk config

--- userdata/DiskConfig.ps1
+++ userdata/DiskConfig.ps1
@@ -17,13 +17,13 @@ Configuration DiskConfig {
       $outfile = ('{0}\log\{1}.diskpart.stdout.log' -f $env:SystemDrive, [DateTime]::Now.ToString("yyyyMMddHHmmss"))
       $errfile = ('{0}\log\{1}.diskpart.stderr.log' -f $env:SystemDrive, [DateTime]::Now.ToString("yyyyMMddHHmmss"))
       $ephemeralVolumeCount = @(Get-WmiObject Win32_Volume | ? { ($_.DriveLetter -and !($_.SystemVolume) -and (-not ($_.DriveLetter -ieq $env:SystemDrive))) }).length
-      $volumeOffset = ($ephemeralVolumeCount - (@(Get-WmiObject Win32_Volume).length))
+      $volumeOffset = ((@(Get-WmiObject Win32_Volume).length) - $ephemeralVolumeCount)
       $diskpartscript = @(
         '',
         "select volume 1`nremove all dismount`nselect disk 1`nclean`nconvert gpt`ncreate partition primary`nformat quick fs=ntfs`nselect volume 1`nassign letter=X",
         "select disk 1`nclean`nconvert dynamic`nselect disk 2`n clean`nconvert dynamic`ncreate volume stripe disk=1,2`nformat quick fs=ntfs`nassign letter=X"
       )
-      if (($ephemeralVolumeCount -gt 1) -and ($ephemeralVolumeCount -lt $diskpartscript.length)) {
+      if (($ephemeralVolumeCount -gt 0) -and ($volumeOffset -gt 0) -and ($ephemeralVolumeCount -lt $diskpartscript.length)) {
         $cs = gwmi Win32_ComputerSystem
         if ($cs.AutomaticManagedPagefile) {
           $cs.AutomaticManagedPagefile = $False
