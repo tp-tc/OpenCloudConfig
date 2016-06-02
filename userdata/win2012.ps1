3c67ae77474c5db765c451cdb6a415a2fc3af9e4
Rob Thijssen <rthijssen@gmail.com>
2016-06-02 12:06:35 +0100

    give g-w a moment to fire up

--- userdata/win2012.ps1
+++ userdata/win2012.ps1
@@ -63,8 +63,11 @@ if($rebootReasons.length) {
     Get-ChildItem -Path ('{0}\log' -f $env:SystemDrive) | ? { !$_.PSIsContainer -and $_.Name.EndsWith('.log') -and $_.FullName -ne $logFile } | % { Remove-Item -Path $_.FullName -Force }
     if ((Get-ChildItem -Path ('{0}\log' -f $env:SystemDrive) | ? { $_.Name.EndsWith('.userdata-run.zip') }).Count -eq 1) {
       & shutdown @('-s', '-t', '0', '-c', 'dsc run complete', '-f', '-d', 'p:4:1') | Out-File -filePath $logFile -append
-    } elseif ((@(Get-Process | ? { $_.ProcessName -eq 'generic-worker' }).length -eq 0) -and (Test-Path -Path 'C:\generic-worker\run-generic-worker.bat' -ErrorAction SilentlyContinue)) {
-      & shutdown @('-r', '-t', '0', '-c', 'starting generic worker', '-f', '-d', 'p:4:1') | Out-File -filePath $logFile -append
+    } elseif (Test-Path -Path 'C:\generic-worker\run-generic-worker.bat' -ErrorAction SilentlyContinue) {
+      Start-Sleep -seconds 30 # give g-w a moment to fire up, if it doesn't, boot loop.
+      if (@(Get-Process | ? { $_.ProcessName -eq 'generic-worker' }).length -eq 0) {
+        & shutdown @('-r', '-t', '0', '-c', 'starting generic worker', '-f', '-d', 'p:4:1') | Out-File -filePath $logFile -append
+      }
     }
   }
 }
