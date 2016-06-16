23c42842b7c004b29df56c401595e74924e54600
Rob Thijssen <rthijssen@gmail.com>
2016-06-16 13:56:49 +0100

    win7, win10

--- userdata/win7.ps1
+++ userdata/win7.ps1
@@ -38,14 +38,12 @@ if ((-not ([string]::IsNullOrWhiteSpace($hostname))) -and (-not ([System.Net.Dns
 }
 if($rebootReasons.length) {
   & shutdown @('-r', '-t', '0', '-c', [string]::Join(', ', $rebootReasons), '-f', '-d', 'p:4:1') | Out-File -filePath $logFile -append
-  #('skipping reboots: {0}' -f [string]::Join(', ', $rebootReasons)) | Out-File -filePath $logFile -append
 } else {
   Start-Transcript -Path $logFile -Append
   Run-RemoteDesiredStateConfig -url 'https://raw.githubusercontent.com/MozRelOps/OpenCloudConfig/master/userdata/DynamicConfig.ps1'
   Stop-Transcript
   if (((Get-Content $logFile) | % { (($_ -match 'requires a reboot') -or ($_ -match 'reboot is required')) }) -contains $true) {
     & shutdown @('-r', '-t', '0', '-c', 'a package installed by dsc requested a restart', '-f', '-d', 'p:4:1') | Out-File -filePath $logFile -append
-    #('skipping reboots: {0}' -f 'a package installed by dsc requested a restart') | Out-File -filePath $logFile -append
   } else {
     # symlink mercurial to ec2 region config. todo: find a way to do this in the manifest (cmd)
     $hgini = ('{0}\python\Scripts\mercurial.ini' -f $env:MozillaBuild)
@@ -56,11 +54,16 @@ if($rebootReasons.length) {
       & cmd @('/c', 'mklink', $hgini, ($hgini.Replace('.ini', ('.{0}.ini' -f ((New-Object Net.WebClient).DownloadString('http://169.254.169.254/latest/meta-data/placement/availability-zone') -replace '.$')))))
     }
     # create a scheduled task
-    if (-not (Get-ScheduledTask -TaskName 'RunDesiredStateConfigurationAtStartup' -ErrorAction SilentlyContinue)) {
-      New-Item -ItemType Directory -Force -Path 'C:\dsc'
-      (New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/MozRelOps/OpenCloudConfig/master/userdata/win7.ps1', 'C:\dsc\win7.ps1')
-      & schtasks @('/create', '/tn', 'RunDesiredStateConfigurationAtStartup', '/sc', 'onstart', '/ru', 'SYSTEM', '/rl', 'HIGHEST', '/tr', 'powershell.exe -File C:\dsc\win7.ps1') | Out-File -filePath $logFile -append
+    $script = 'win7.ps1'
+    if (Test-Path -Path ('C:\dsc\{0}' -f $script) -ErrorAction SilentlyContinue) {
+      Remove-Item -Path ('C:\dsc\{0}' -f $script) -Force
     }
+    New-Item -ItemType Directory -Force -Path 'C:\dsc'
+      (New-Object Net.WebClient).DownloadFile(('https://raw.githubusercontent.com/MozRelOps/OpenCloudConfig/master/userdata/{0}' -f $script), ('C:\dsc\{0}' -f $script))
+    if (-not (Get-ScheduledTask -TaskName 'RunDesiredStateConfigurationAtStartup' -ErrorAction SilentlyContinue)) {
+      & schtasks @('/create', '/tn', 'RunDesiredStateConfigurationAtStartup', '/sc', 'onstart', '/ru', 'SYSTEM', '/rl', 'HIGHEST', '/tr', ('powershell.exe -File C:\dsc\{0}' -f $script)) | Out-File -filePath $logFile -append
+    }
+    
     Get-ChildItem -Path ('{0}\log' -f $env:SystemDrive) | ? { !$_.PSIsContainer -and $_.Name.EndsWith('.log') -and $_.Length -eq 0 } | % { Remove-Item -Path $_.FullName -Force }
     New-ZipFile -ZipFilePath $logFile.Replace('.log', '.zip') -Item @(Get-ChildItem -Path ('{0}\log' -f $env:SystemDrive) | ? { !$_.PSIsContainer -and $_.Name.EndsWith('.log') -and $_.FullName -ne $logFile } | % { $_.FullName })
     Get-ChildItem -Path ('{0}\log' -f $env:SystemDrive) | ? { !$_.PSIsContainer -and $_.Name.EndsWith('.log') -and $_.FullName -ne $logFile } | % { Remove-Item -Path $_.FullName -Force }
@@ -70,7 +73,6 @@ if($rebootReasons.length) {
       Start-Sleep -seconds 30 # give g-w a moment to fire up, if it doesn't, boot loop.
       if (@(Get-Process | ? { $_.ProcessName -eq 'generic-worker' }).length -eq 0) {
         & shutdown @('-r', '-t', '0', '-c', 'starting generic worker', '-f', '-d', 'p:4:1') | Out-File -filePath $logFile -append
-        #('skipping reboots: {0}' -f 'starting generic worker') | Out-File -filePath $logFile -append
       }
     }
   }
