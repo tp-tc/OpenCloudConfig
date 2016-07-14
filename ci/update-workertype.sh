0787705ceb3dd0ed064635e58d5c2e4348d4c8e3
Rob Thijssen <rthijssen@gmail.com>
2016-07-14 14:31:41 +0100

    test secret use

--- ci/update-workertype.sh
+++ ci/update-workertype.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 updateworkertype_secrets_url="taskcluster/secrets/v1/secret/repo:github.com/mozilla-releng/OpenCloudConfig:updateworkertype"
-TASKCLUSTER_CLIENT_ID=$(curl ${password_url} | python -c 'import json, sys; a = json.load(sys.stdin); print a["secret"]["TASKCLUSTER_CLIENT_ID"]')
+TASKCLUSTER_CLIENT_ID=$(curl ${updateworkertype_secrets_url} | python -c 'import json, sys; a = json.load(sys.stdin); print a["secret"]["TASKCLUSTER_CLIENT_ID"]')
 
 echo "TASKCLUSTER_CLIENT_ID: $TASKCLUSTER_CLIENT_ID"
