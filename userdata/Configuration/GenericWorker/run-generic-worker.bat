e5965de768f15d2700eb8b40b0fa216e03e3d314
Rob Thijssen <rthijssen@gmail.com>
2016-10-29 19:01:02 +0100

    remove ready state semaphore before starting g-w

--- userdata/Configuration/GenericWorker/run-generic-worker.bat
+++ userdata/Configuration/GenericWorker/run-generic-worker.bat
@@ -6,5 +6,6 @@ timeout /t 1 >nul
 goto CheckForStateFlag
 
 :RunWorker
+del /Q /F C:\dsc\task-claim-state.valid
 pushd %~dp0
 .\generic-worker.exe run --configure-for-aws > .\generic-worker.log 2>&1
