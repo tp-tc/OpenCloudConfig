7d63de3b7cc2f8c7dd0e4609e3e83f0fe86adfb3
Rob Thijssen <rthijssen@gmail.com>
2016-11-16 17:19:54 +0000

    use c4.2xlarge for win7 on new base ami with ebs cache and task volumes

--- ci/update-workertype.sh
+++ ci/update-workertype.sh
@@ -32,7 +32,7 @@ echo "[opencloudconfig $(date --utc +"%F %T.%3NZ")] git sha: ${aws_client_token}
 case "${tc_worker_type}" in
   *-win7-32*)
     aws_base_ami_search_term=${aws_base_ami_search_term:='gecko-t-win7-32-base-*'}
-    aws_instance_type=${aws_instance_type:='c3.2xlarge'}
+    aws_instance_type=${aws_instance_type:='c4.2xlarge'}
     aws_instance_hdd_size=${aws_instance_hdd_size:=120}
     aws_base_ami_id="$(aws ec2 describe-images --region ${aws_region} --owners self --filters "Name=state,Values=available" "Name=name,Values=${aws_base_ami_search_term}" --query 'Images[*].{A:CreationDate,B:ImageId}' --output text | sort -u | tail -1 | cut -f2)"
     ami_description="Gecko test worker for Windows 7 32 bit; TaskCluster worker: ${tc_worker_type}, version ${aws_client_token}, https://github.com/mozilla-releng/OpenCloudConfig/tree/${GITHUB_HEAD_SHA}"}
