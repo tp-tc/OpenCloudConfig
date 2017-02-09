6f97ba98791d35cef9d694f7ec968ce142798ff3
Rob Thijssen <rthijssen@gmail.com>
2017-02-09 17:56:44 +0000

    handle existing %USERPROFILE%\AppData not in task dir

--- userdata/Configuration/GenericWorker/task-user-init-win10.cmd
+++ userdata/Configuration/GenericWorker/task-user-init-win10.cmd
@@ -1,3 +1,2 @@
-whoami
-set
+if "%cd%" NEQ "%USERPROFILE%" if exist %USERPROFILE%\AppData if not exist %cd%\AppData mklink /d %cd%\AppData %USERPROFILE%\AppData
 powershell -command "& {& Add-AppxPackage -DisableDevelopmentMode -Register C:\Windows\SystemApps\Microsoft.MicrosoftEdge_8wekyb3d8bbwe\AppXManifest.xml -Verbose }"
