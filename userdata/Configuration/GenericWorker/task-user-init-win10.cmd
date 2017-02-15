f4bce2beef4fcad96da74ce43aca85d6e873e375
Rob Thijssen <rthijssen@gmail.com>
2017-02-15 13:17:58 +0000

    bug 1261188 - copy existing profile to task profile dir

--- userdata/Configuration/GenericWorker/task-user-init-win10.cmd
+++ userdata/Configuration/GenericWorker/task-user-init-win10.cmd
@@ -1,2 +1,3 @@
-if "%cd%" NEQ "%USERPROFILE%" if exist %USERPROFILE%\AppData if not exist %cd%\AppData mklink /d %cd%\AppData %USERPROFILE%\AppData
+robocopy %USERPROFILE%\AppData\Local %cd%\AppData\Local /mir /sec /xjd /w:1 /r:1
+robocopy %USERPROFILE%\AppData\Roaming %cd%\AppData\Roaming /mir /sec /xjd /w:1 /r:1
 powershell -command "& {& Add-AppxPackage -DisableDevelopmentMode -Register C:\Windows\SystemApps\Microsoft.MicrosoftEdge_8wekyb3d8bbwe\AppXManifest.xml -Verbose }"
