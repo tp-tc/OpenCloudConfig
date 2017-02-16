e11f23f0cff0d13a3e8a221ff73b72753474e986
Rob Thijssen <rthijssen@gmail.com>
2017-02-16 13:14:38 +0000

    revert to AppData in UserProfile on Win 10. deploy: gecko-t-win10-64 gecko-t-win10-64-gpu

--- userdata/Configuration/GenericWorker/task-user-init-win10.cmd
+++ userdata/Configuration/GenericWorker/task-user-init-win10.cmd
@@ -4,10 +4,6 @@ set AppData=%USERPROFILE%\AppData\Roaming
 setx AppData %USERPROFILE%\AppData\Roaming
 set LocalAppData=%USERPROFILE%\AppData\Local
 setx LocalAppData %USERPROFILE%\AppData\Local
-powershell -command "& {& [Environment]::SetEnvironmentVariable('AppData', ('{0}\AppData\Roaming' -f $env:USERPROFILE), 'Process') }"
-powershell -command "& {& [Environment]::SetEnvironmentVariable('AppData', ('{0}\AppData\Roaming' -f $env:USERPROFILE), 'User') }"
-powershell -command "& {& [Environment]::SetEnvironmentVariable('LocalAppData', ('{0}\AppData\Local' -f $env:USERPROFILE), 'Process') }"
-powershell -command "& {& [Environment]::SetEnvironmentVariable('LocalAppData', ('{0}\AppData\Local' -f $env:USERPROFILE), 'User') }"
 powershell -command "& {& Set-KnownFolderPath -KnownFolder RoamingAppData -Path $env:AppData }"
 powershell -command "& {& Set-KnownFolderPath -KnownFolder LocalAppData -Path $env:LocalAppData }"
 powershell -command "& {& Add-AppxPackage -DisableDevelopmentMode -Register C:\Windows\SystemApps\Microsoft.MicrosoftEdge_8wekyb3d8bbwe\AppXManifest.xml -Verbose }"
