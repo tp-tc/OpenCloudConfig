19aade635ffab7b2524120883737aad587e8b497
Rob Thijssen <rthijssen@gmail.com>
2017-05-03 14:58:23 +0300

    install NVidia Grid K520 drivers on Win 10 GPU instances
    nodeploy

--- ci/update-workertype.sh
+++ ci/update-workertype.sh
@@ -55,7 +55,7 @@ echo "[opencloudconfig $(date --utc +"%F %T.%3NZ")] git sha: ${aws_client_token}
 case "${tc_worker_type}" in
   @(gecko|loan)-t-win7-32-gpu*)
     aws_base_ami_search_term=${aws_base_ami_search_term:='gecko-t-win7-32-base-20170403-releng-activated'}
-    aws_instance_type=${aws_instance_type:='c4.2xlarge'}
+    aws_instance_type=${aws_instance_type:='g2.2xlarge'}
     aws_instance_hdd_size=${aws_instance_hdd_size:=30}
     aws_base_ami_id="$(aws ec2 describe-images --region ${aws_region} --owners self --filters "Name=state,Values=available" "Name=name,Values=${aws_base_ami_search_term}" --query 'Images[*].{A:CreationDate,B:ImageId}' --output text | sort -u | tail -1 | cut -f2)"
     ami_description="Gecko test worker for Windows 7 32 bit; TaskCluster worker type: ${tc_worker_type}, OCC version ${aws_client_token}, https://github.com/mozilla-releng/OpenCloudConfig/tree/${GITHUB_HEAD_SHA}"}
@@ -77,7 +77,7 @@ case "${tc_worker_type}" in
     ;;
   @(gecko|loan)-t-win10-64-gpu*)
     aws_base_ami_search_term=${aws_base_ami_search_term:='gecko-t-win10-64-base-*'}
-    aws_instance_type=${aws_instance_type:='c4.2xlarge'}
+    aws_instance_type=${aws_instance_type:='g2.2xlarge'}
     aws_instance_hdd_size=${aws_instance_hdd_size:=120}
     aws_base_ami_id="$(aws ec2 describe-images --region ${aws_region} --owners self --filters "Name=state,Values=available" "Name=name,Values=${aws_base_ami_search_term}" --query 'Images[*].{A:CreationDate,B:ImageId}' --output text | sort -u | tail -1 | cut -f2)"
     ami_description="Gecko tester for Windows 10 64 bit; TaskCluster worker type: ${tc_worker_type}, OCC version ${aws_client_token}, https://github.com/mozilla-releng/OpenCloudConfig/tree/${GITHUB_HEAD_SHA}"}
