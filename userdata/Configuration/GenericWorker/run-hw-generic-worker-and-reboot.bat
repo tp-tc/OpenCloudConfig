26010943c1def56d0c30ef24638ca1bb34205ba8
markcor <mcornmesser@mozilla.com>
2017-06-14 08:30:37 -0700

    Change the log redirect to append.

--- userdata/Configuration/GenericWorker/run-hw-generic-worker-and-reboot.bat
+++ userdata/Configuration/GenericWorker/run-hw-generic-worker-and-reboot.bat
@@ -10,6 +10,6 @@ goto CheckForStateFlag
 :RunWorker
 del /Q /F C:\dsc\task-claim-state.valid
 pushd %~dp0
-C:\generic-worker\generic-worker.exe run --config C:\generic-worker\gen_worker.config > C:\generic-worker\generic-worker.log 2>&1
+C:\generic-worker\generic-worker.exe run --config C:\generic-worker\gen_worker.config >> C:\generic-worker\generic-worker.log 2>&1
 
 shutdown /r /t 0 /f /c "Rebooting as generic worker ran successfully"
