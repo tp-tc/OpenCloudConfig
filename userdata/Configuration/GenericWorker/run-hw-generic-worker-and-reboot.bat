34c5e02cf7570202501fe8b32a5848a3f42a59ca
markcor <mcornmesser@mozilla.com>
2017-06-19 10:03:53 -0700

    Add task-claim-state.valid file creation

--- userdata/Configuration/GenericWorker/run-hw-generic-worker-and-reboot.bat
+++ userdata/Configuration/GenericWorker/run-hw-generic-worker-and-reboot.bat
@@ -12,4 +12,5 @@ del /Q /F C:\dsc\task-claim-state.valid
 pushd %~dp0
 C:\generic-worker\generic-worker.exe run --config C:\generic-worker\gen_worker.config >> C:\generic-worker\generic-worker.log 2>&1
 
+<nul (set/p z=) >C:\dsc\task-claim-state.valid
 shutdown /r /t 0 /f /c "Rebooting as generic worker ran successfully"
