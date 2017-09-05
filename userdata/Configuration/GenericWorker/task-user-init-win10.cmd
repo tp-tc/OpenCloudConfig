5d58b5bad3f46749873ae39f086100f9867d05c8
Rob Thijssen <rthijssen@gmail.com>
2017-09-05 10:53:29 +0300

    Bug 1373178 - auto hide win 10 task bar

--- userdata/Configuration/GenericWorker/task-user-init-win10.cmd
+++ userdata/Configuration/GenericWorker/task-user-init-win10.cmd
@@ -1,9 +1,4 @@
-rd /s /q %cd%\AppData
-mklink /d %cd%\AppData %USERPROFILE%\AppData
-set AppData=%USERPROFILE%\AppData\Roaming
-setx AppData %USERPROFILE%\AppData\Roaming
-set LocalAppData=%USERPROFILE%\AppData\Local
-setx LocalAppData %USERPROFILE%\AppData\Local
-powershell -command "& {& Set-KnownFolderPath -KnownFolder RoamingAppData -Path $env:AppData }"
-powershell -command "& {& Set-KnownFolderPath -KnownFolder LocalAppData -Path $env:LocalAppData }"
-powershell -command "& {& Add-AppxPackage -DisableDevelopmentMode -Register C:\Windows\SystemApps\Microsoft.MicrosoftEdge_8wekyb3d8bbwe\AppXManifest.xml -Verbose }"
+rem task user initialisation script
+
+rem auto hide task bar
+powershell -command "&{$p='HKCU:SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\StuckRects3';$v=(Get-ItemProperty -Path $p).Settings;$v[8]=3;&Set-ItemProperty -Path $p -Name Settings -Value $v;&Stop-Process -ProcessName explorer}"
