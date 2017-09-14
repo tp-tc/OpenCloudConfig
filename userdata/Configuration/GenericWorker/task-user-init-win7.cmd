d67839dcfda20f096c88d4c640910b805efb80e6
Rob Thijssen <rthijssen@gmail.com>
2017-09-14 10:34:47 +0300

    bug 1396168 - firewall exceptions for task user apps (#91)

--- userdata/Configuration/GenericWorker/task-user-init-win7.cmd
+++ userdata/Configuration/GenericWorker/task-user-init-win7.cmd
@@ -1,8 +1,5 @@
 rem task user initialisation script
 
-rem auto hide task bar
-powershell -command "&{$p='HKCU:SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\StuckRects3';$v=(Get-ItemProperty -Path $p).Settings;$v[8]=3;&Set-ItemProperty -Path $p -Name Settings -Value $v;&Stop-Process -ProcessName explorer}"
-
 rem task user firewall exceptions
 netsh advfirewall firewall add rule name="ssltunnel-%USERNAME%" dir=in action=allow program="%USERPROFILE%\build\tests\bin\ssltunnel.exe" enable=yes
 netsh advfirewall firewall add rule name="ssltunnel-%USERNAME%" dir=out action=allow program="%USERPROFILE%\build\tests\bin\ssltunnel.exe" enable=yes
