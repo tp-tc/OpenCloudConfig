63775083a0f037a348c3ce3e8f7c7cf877c22c95
markcor <mcornmesser@mozilla.com>
2017-10-12 23:34:35 -0700

    Update xDynamicConfig.ps1

--- userdata/xDynamicConfig.ps1
+++ userdata/xDynamicConfig.ps1
@@ -8,7 +8,7 @@ Configuration xDynamicConfig {
   Import-DscResource -ModuleName PSDesiredStateConfiguration,xPSDesiredStateConfiguration,xWindowsUpdate
   # sourceRepo is in place to toggle between production and testing environments
   #$sourceRepo = 'mozilla-releng'
-	$sourceRepo = 'markcor'
+
   if ((Get-Service 'Ec2Config' -ErrorAction SilentlyContinue) -or (Get-Service 'AmazonSSMAgent' -ErrorAction SilentlyContinue)) {
     $locationType = 'AWS'
   } else {
