7efffadba63934ed9c3b590cf23febd184b8c009
markcor <mcornmesser@mozilla.com>
2018-06-14 12:36:08 -0700

    Update rundsc.ps1

--- userdata/rundsc.ps1
+++ userdata/rundsc.ps1
@@ -972,7 +972,6 @@ Set-DefaultStrongCryptography
 
 # SourceRepo is in place to toggle between production and testing environments
 $SourceRepo = 'mozilla-releng'
-$SourceRepo = 'markcor'
 
 # The Windows update service needs to be enabled for OCC to process but needs to be disabled during testing. 
 $UpdateService = Get-Service -Name wuauserv
