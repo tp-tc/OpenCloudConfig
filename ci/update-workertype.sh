d1969a3aef09ffa69dc2b7dfd898e336a8b65665
Rob Thijssen <rthijssen@gmail.com>
2018-07-17 10:12:32 +0300

    Bug 1469398 - rebuild l3 ami for ena
    deploy: gecko-3-b-win2012

--- ci/update-workertype.sh
+++ ci/update-workertype.sh
@@ -123,19 +123,9 @@ case "${tc_worker_type}" in
     worker_username=GenericWorker
     block_device_mappings='[{"DeviceName":"/dev/sda1","Ebs":{"VolumeType":"gp2","VolumeSize":120,"DeleteOnTermination":true}},{"DeviceName":"/dev/sdb","Ebs":{"VolumeType":"gp2","VolumeSize":120,"DeleteOnTermination":true}}]'
     ;;
-  gecko-[12]-b-win2012*)
+  gecko-[123]-b-win2012*)
     aws_base_ami_search_term=${aws_base_ami_search_term:='gecko-b-win2012-ena-base-*'}
     aws_instance_type=${aws_instance_type:='c5.4xlarge'}
-    aws_base_ami_id="$(aws ec2 describe-images --region ${aws_region} --owners self --filters "Name=state,Values=available" "Name=name,Values=${aws_base_ami_search_term}" --query 'Images[*].{A:CreationDate,B:ImageId}' --output text | sort -u | tail -1 | cut -f2)"
-    ami_description="Gecko builder for Windows; TaskCluster worker type: ${tc_worker_type}, OCC version ${aws_client_token}, https://github.com/mozilla-releng/OpenCloudConfig/tree/${GITHUB_HEAD_SHA}"}
-    gw_tasks_dir='Z:\'
-    root_username=Administrator
-    worker_username=GenericWorker
-    block_device_mappings='[{"DeviceName":"/dev/sda1","Ebs":{"VolumeType":"gp2","VolumeSize":40,"DeleteOnTermination":true}},{"DeviceName":"/dev/sdb","Ebs":{"VolumeType":"gp2","VolumeSize":120,"DeleteOnTermination":true}}]'
-    ;;
-  gecko-3-b-win2012)
-    aws_base_ami_search_term=${aws_base_ami_search_term:='gecko-b-win2012-base-*'}
-    aws_instance_type=${aws_instance_type:='c4.4xlarge'}
     aws_base_ami_id="$(aws ec2 describe-images --region ${aws_region} --owners self --filters "Name=state,Values=available" "Name=name,Values=${aws_base_ami_search_term}" --query 'Images[*].{A:CreationDate,B:ImageId}' --output text | sort -u | tail -1 | cut -f2)"
     ami_description="Gecko builder for Windows; TaskCluster worker type: ${tc_worker_type}, OCC version ${aws_client_token}, https://github.com/mozilla-releng/OpenCloudConfig/tree/${GITHUB_HEAD_SHA}"}
     gw_tasks_dir='Z:\'
