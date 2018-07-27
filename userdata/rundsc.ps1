10d5e2a64d6cb5c5b215d318400ae5f37ea4a824
markcor <mcornmesser@mozilla.com>
2018-07-27 10:38:18 -0700

    Update rundsc.ps1

--- userdata/rundsc.ps1
+++ userdata/rundsc.ps1
@@ -1217,7 +1217,7 @@ if ($rebootReasons.length) {
       }
     }
     Set-ExecutionPolicy RemoteSigned -force | Out-File -filePath $logFile -append
-    & cmd @('/c', 'winrm', 'set', 'winrm/config', '@{MaxEnvelopeSizekb="16384"}')
+    & cmd @('/c', 'winrm', 'set', 'winrm/config', '@{MaxEnvelopeSizekb="32696"}')
     $transcript = ('{0}\log\{1}.dsc-run.log' -f $env:SystemDrive, [DateTime]::Now.ToString("yyyyMMddHHmmss"))
     # end pre dsc setup ###########################################################################################################################################
 
