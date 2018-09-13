3c13c155d414ff78485e0c6264355d3f08b100e7
Rob Thijssen <rthijssen@gmail.com>
2018-09-13 21:38:41 +0300

    Bug 1474548 - test automation ami
    deploy: gecko-t-win10-64-beta gecko-t-win10-64-gpu-b

--- ci/update-workertype.sh
+++ ci/update-workertype.sh
@@ -99,6 +99,14 @@ case "${tc_worker_type}" in
     root_username=root
     worker_username=GenericWorker
     ;;
+  gecko-t-win10-64-gpu-b|gecko-t-win10-64-beta)
+    aws_base_ami_search_term=${aws_base_ami_search_term:='Windows_10_Professional_1803_17134_en-US_x64_MBR'}
+    aws_base_ami_id="$(aws ec2 describe-images --region ${aws_region} --owners self --filters "Name=state,Values=available" "Name=name,Values=${aws_base_ami_search_term}" --query 'Images[*].{A:CreationDate,B:ImageId}' --output text | sort -u | tail -1 | cut -f2)"
+    ami_description="Gecko tester for Windows 10 64 bit; TaskCluster worker type: ${tc_worker_type}, OCC version ${aws_client_token}, https://github.com/mozilla-releng/OpenCloudConfig/tree/${GITHUB_HEAD_SHA}"}
+    gw_tasks_dir='Z:\'
+    root_username=Administrator
+    worker_username=GenericWorker
+    ;;
   gecko-t-win10-64-gpu*)
     aws_base_ami_search_term=${aws_base_ami_search_term:='gecko-t-win10-64-gpu-base-20180320'}
     aws_base_ami_id="$(aws ec2 describe-images --region ${aws_region} --owners self --filters "Name=state,Values=available" "Name=name,Values=${aws_base_ami_search_term}" --query 'Images[*].{A:CreationDate,B:ImageId}' --output text | sort -u | tail -1 | cut -f2)"
