1f8d49770bd7c88381a4a611b8e27db0e8d7cb53
markcor <mcornmesser@mozilla.com>
2018-10-09 11:49:31 -0700

    bug1493759 - Adding Dcom loging

--- userdata/rundsc.ps1
+++ userdata/rundsc.ps1
@@ -1014,7 +1014,7 @@ function hw-DiskManage {
 Set-DefaultStrongCryptography
 
 # SourceRepo is in place to toggle between production and testing environments
-$SourceRepo = 'mozilla-releng'
+$SourceRepo = 'markcor'
 
 # The Windows update service needs to be enabled for OCC to process but needs to be disabled during testing. 
 $UpdateService = Get-Service -Name wuauserv
