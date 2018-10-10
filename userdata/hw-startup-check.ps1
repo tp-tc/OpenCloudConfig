01944024676d6506e87737b1f996de936e3d773e
markcor <mcornmesser@mozilla.com>
2018-10-10 15:37:01 -0700

    Update hw-startup-check.ps1

--- userdata/hw-startup-check.ps1
+++ userdata/hw-startup-check.ps1
@@ -1,17 +1,17 @@
 [string[]] $flags = @(
-  ('{0}\dsc\in-progress.lock' -f $env:SystemDrive),
-  ('{0}\dsc\EndOfManifest.semaphore' -f $env:SystemRoot),
-  ('{0}\dsc\task-claim-state.valid' -f $env:SystemDrive)
+  ('{0}\\dsc\\in-progress.lock' -f $env:SystemDrive),
+  ('{0}\\dsc\\EndOfManifest.semaphore' -f $env:SystemRoot),
+  ('{0}\\dsc\\task-claim-state.valid' -f $env:SystemDrive)
 )
-$rundsc = '{0}:\dsc\rundsc.ps1'-f $env:SystemDrive
+$rundsc = '{0}:\dsc\\rundsc.ps1'-f $env:SystemDrive
 $GWProcess = Get-Process generic-worker -ErrorAction SilentlyContinue
 
 write-host $flags
 write-host $rundsc
 
 if (!(Test-Path $rundsc ) -Or ((Get-Content $rundsc) -eq $Null) ) {
-  (New-Object Net.WebClient).DownloadFile(("https://raw.githubusercontent.com/markcor/OpenCloudConfig/master/userdata/rundsc.ps1?{0}" -f [Guid]::NewGuid()), '$rundsc')
-  while (!(Test-Path '{0}:\dsc\rundsc.ps1'-f $env:SystemDrive))
+  (New-Object Net.WebClient).DownloadFile("https://raw.githubusercontent.com/markcor/OpenCloudConfig/master/userdata/rundsc.ps1", "$rundsc")
+  while (!(Test-Path $rundsc)){ Start-Sleep 10 }
   foreach ($flag in $flagss) {
     if (Test-Path -Path $flag -ErrorAction SilentlyContinue) {
     Remove-Item $flag -confirm:$false -recurse:$true -force -ErrorAction SilentlyContinue
