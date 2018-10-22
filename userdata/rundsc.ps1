f6361726e3db8bf84d34559e2202c7fc0162cda0
Rob Thijssen <rthijssen@gmail.com>
2018-10-22 14:11:49 +0300

    Bug 1500877 - remove support for deprecated loaner functionality

--- userdata/rundsc.ps1
+++ userdata/rundsc.ps1
@@ -1385,8 +1385,6 @@ if ($rebootReasons.length) {
     # create a scheduled task to run HaltOnIdle every 2 minutes
     Create-ScheduledPowershellTask -taskName 'HaltOnIdle' -scriptUrl ('https://raw.githubusercontent.com/{0}/OpenCloudConfig/master/userdata/HaltOnIdle.ps1?{1}' -f $SourceRepo, [Guid]::NewGuid()) -scriptPath 'C:\dsc\HaltOnIdle.ps1' -sc 'minute' -mo '2'
   }
-  # create a scheduled task to run PrepLoaner every minute (only preps loaner if appropriate flags exist. flags are created by user tasks)
-  Create-ScheduledPowershellTask -taskName 'PrepLoaner' -scriptUrl ('https://raw.githubusercontent.com/{0}/OpenCloudConfig/master/userdata/PrepLoaner.ps1?{1}' -f $SourceRepo, [Guid]::NewGuid()) -scriptPath 'C:\dsc\PrepLoaner.ps1' -sc 'minute' -mo '1'
   # create a scheduled task to run system maintenance on startup
   Create-ScheduledPowershellTask -taskName 'MaintainSystem' -scriptUrl ('https://raw.githubusercontent.com/{0}/OpenCloudConfig/master/userdata/MaintainSystem.ps1?{1}' -f $SourceRepo, [Guid]::NewGuid()) -scriptPath 'C:\dsc\MaintainSystem.ps1' -sc 'onstart'
   if ($locationType -eq 'DataCenter') {
