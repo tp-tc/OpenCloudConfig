3a503863739e15d19538a07249ab6c6c115fa7ef
Rob Thijssen <rthijssen@gmail.com>
2018-10-26 16:12:27 +0300

    Bug 1501609 - reduce log noise from system user powershell runs

--- userdata/Configuration/Microsoft.PowerShell_profile.ps1
+++ userdata/Configuration/Microsoft.PowerShell_profile.ps1
@@ -1,32 +1,38 @@
 
 # show file extensions, show hidden files, hide os files in explorer
 $explorerRegKey = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced'
-switch -wildcard ((Get-WmiObject -class Win32_OperatingSystem).Caption) {
-  'Microsoft Windows 7*' {
-    Set-ItemProperty $explorerRegKey -Type 'DWord' -Name 'Hidden' -Value 1
-    Set-ItemProperty $explorerRegKey -Type 'DWord' -Name 'HideFileExt' -Value 0
-    Set-ItemProperty $explorerRegKey -Type 'DWord' -Name 'ShowSuperHidden' -Value 0
-  }
-  default {
-    Set-ItemProperty $explorerRegKey -Type 'DWord' -Name 'HideFileExt' -Value '0x00000002' # off
-    Set-ItemProperty $explorerRegKey -Type 'DWord' -Name 'Hidden' -Value '0x00000001'
-    Set-ItemProperty $explorerRegKey -Type 'DWord' -Name 'ShowSuperHidden' -Value '0x00000000'
+if (Get-ItemProperty $explorerRegKey -ErrorAction SilentlyContinue) {
+  switch -wildcard ((Get-WmiObject -class Win32_OperatingSystem).Caption) {
+    'Microsoft Windows 7*' {
+      Set-ItemProperty $explorerRegKey -Type 'DWord' -Name 'Hidden' -Value 1
+      Set-ItemProperty $explorerRegKey -Type 'DWord' -Name 'HideFileExt' -Value 0
+      Set-ItemProperty $explorerRegKey -Type 'DWord' -Name 'ShowSuperHidden' -Value 0
+    }
+    default {
+      Set-ItemProperty $explorerRegKey -Type 'DWord' -Name 'HideFileExt' -Value '0x00000002' # off
+      Set-ItemProperty $explorerRegKey -Type 'DWord' -Name 'Hidden' -Value '0x00000001'
+      Set-ItemProperty $explorerRegKey -Type 'DWord' -Name 'ShowSuperHidden' -Value '0x00000000'
+    }
   }
 }
 
 # a large console, with a large screen buffer (for reading build logs)
-Set-ItemProperty 'HKCU:\Console\' -Type 'DWord' -Name 'QuickEdit' -Value '0x00000001' # on
-Set-ItemProperty 'HKCU:\Console\' -Type 'DWord' -Name 'InsertMode' -Value '0x00000001' # on
-Set-ItemProperty 'HKCU:\Console\' -Type 'DWord' -Name 'ScreenBufferSize' -Value '0x0bb800a0' # 160x3000
-Set-ItemProperty 'HKCU:\Console\' -Type 'DWord' -Name 'WindowSize' -Value '0x003c00a0' # 160x60
-Set-ItemProperty 'HKCU:\Console\' -Type 'DWord' -Name 'HistoryBufferSize' -Value '0x000003e7' # 999 (max)
-Set-ItemProperty 'HKCU:\Console\' -Type 'DWord' -Name 'ScreenColors' -Value '0x0000000a' # green on black
-Set-ItemProperty 'HKCU:\Console\' -Type 'DWord' -Name 'FontSize' -Value '0x000c0000' # 12
-Set-ItemProperty 'HKCU:\Console\' -Type 'DWord' -Name 'FontFamily' -Value '0x00000036' # default console fonts
-Set-ItemProperty 'HKCU:\Console\' -Type 'String' -Name 'FaceName' -Value 'Lucida Console'
+if (Get-ItemProperty 'HKCU:\Console\' -ErrorAction SilentlyContinue) {
+  Set-ItemProperty 'HKCU:\Console\' -Type 'DWord' -Name 'QuickEdit' -Value '0x00000001' # on
+  Set-ItemProperty 'HKCU:\Console\' -Type 'DWord' -Name 'InsertMode' -Value '0x00000001' # on
+  Set-ItemProperty 'HKCU:\Console\' -Type 'DWord' -Name 'ScreenBufferSize' -Value '0x0bb800a0' # 160x3000
+  Set-ItemProperty 'HKCU:\Console\' -Type 'DWord' -Name 'WindowSize' -Value '0x003c00a0' # 160x60
+  Set-ItemProperty 'HKCU:\Console\' -Type 'DWord' -Name 'HistoryBufferSize' -Value '0x000003e7' # 999 (max)
+  Set-ItemProperty 'HKCU:\Console\' -Type 'DWord' -Name 'ScreenColors' -Value '0x0000000a' # green on black
+  Set-ItemProperty 'HKCU:\Console\' -Type 'DWord' -Name 'FontSize' -Value '0x000c0000' # 12
+  Set-ItemProperty 'HKCU:\Console\' -Type 'DWord' -Name 'FontFamily' -Value '0x00000036' # default console fonts
+  Set-ItemProperty 'HKCU:\Console\' -Type 'String' -Name 'FaceName' -Value 'Lucida Console'
+}
 
 # a visible cursor on dark backgrounds (as well as light)
-Set-ItemProperty 'HKCU:\Control Panel\Cursors\' -Type 'String' -Name 'IBeam' -Value '%SYSTEMROOT%\Cursors\beam_r.cur'
+if (Get-ItemProperty 'HKCU:\Control Panel\Cursors\' -ErrorAction SilentlyContinue) {
+  Set-ItemProperty 'HKCU:\Control Panel\Cursors\' -Type 'String' -Name 'IBeam' -Value '%SYSTEMROOT%\Cursors\beam_r.cur'
+}
 
 # powershell, cmd, event viewer and subl pinned to taskbar
 ((New-Object -c Shell.Application).Namespace('{0}\System32\WindowsPowerShell\v1.0' -f $env:SystemRoot).parsename('powershell.exe')).InvokeVerb('taskbarpin')
