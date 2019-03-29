ec9a808b49b7af2da155dfb3f2902050d15540e1
Rob Thijssen <rthijssen@gmail.com>
2019-03-29 11:35:01 +0200

    delete idle instances

--- ci/gcloud-init.sh
+++ ci/gcloud-init.sh
@@ -103,6 +103,23 @@ for manifest in $(ls ${script_dir}/../userdata/Manifest/*-gamma.json); do
     #  fi
     #done
   fi
+  # delete instances that have not taken a task within the idle threshold. note that the tc queue may return instances that have long since terminated
+  idleThreshold=$(date --date "120 minutes ago" +%s)
+  for instance in $(curl -s "https://queue.taskcluster.net/v1/provisioners/${provisionerId}/worker-types/${workerType}/workers" | jq -r '.workers[] | select(.latestTask != null) | @base64'); do
+    _jq() {
+      echo ${instance} | base64 --decode | jq -r ${1}
+    }
+    zoneUrl=$(gcloud compute instances list --filter="name:$(_jq '.workerId') AND zone~$(_jq '.workerGroup')" --format=json | jq -r '.[0].zone')
+    if [ -n "${zoneUrl}" ] && [[ "${zoneUrl}" != "null" ]]; then
+      zone=${zoneUrl##*/}
+      latestResolvedTaskTimeInUtc=$(curl -s "https://queue.taskcluster.net/v1/task/$(_jq '.latestTask.taskId')/status" | jq --arg runId $(_jq '.latestTask.runId') -r '.status.runs[] | select(.runId == ($runId | tonumber)) | .resolved')
+      latestResolvedTaskTime=$(date --date "${latestResolvedTaskTimeInUtc}" +%s)
+      echo "$(tput dim)[${script_name} $(date --utc +"%F %T.%3NZ")]$(tput sgr0) ${zone}/$(_jq '.workerId') last resolved task: $(tput bold)${latestResolvedTaskTimeInUtc}$(tput sgr0) ($(( ($(date +%s) - $latestResolvedTaskTime) / 60)) minutes ago)"
+      if [ ${latestResolvedTaskTime} -lt ${idleThreshold} ] && gcloud compute instances delete $(_jq '.workerId') --zone ${zone} --delete-disks all --quiet; then
+        echo "$(tput dim)[${script_name} $(date --utc +"%F %T.%3NZ")]$(tput sgr0) deleted: $(tput bold)${zone}/$(_jq '.workerId')$(tput sgr0)"
+      fi
+    fi
+  done
 done
 # delete orphaned disks
 for disk in $(gcloud compute disks list --filter=-users:* --format json | jq -r '.[] | @base64'); do
