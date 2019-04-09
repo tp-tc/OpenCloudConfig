f3d9dc12c8c7e4339d69316f731abc40567ca354
Rob Thijssen <rthijssen@gmail.com>
2019-04-09 18:26:19 +0300

    configure dpkg to leave old config

--- ci/gcloud-init-provisioner.sh
+++ ci/gcloud-init-provisioner.sh
@@ -6,9 +6,9 @@ log_dir=/var/log/releng-gcp-provisioner
 sudo mkdir ${log_dir}
 sudo chown ${USER}:${USER} ${log_dir}
 #sudo chmod -R o+r ${log_dir}/
-curl -sLO https://github.com/papertrail/remote_syslog2/releases/download/v0.20/remote-syslog2_0.20_amd64.deb
-sudo dpkg -i remote-syslog2_0.20_amd64.deb
 sudo curl -sL -o /etc/log_files.yml https://raw.githubusercontent.com/mozilla-releng/OpenCloudConfig/gamma/ci/gcloud-provisioner-log-config.yml
+curl -sLO https://github.com/papertrail/remote_syslog2/releases/download/v0.20/remote-syslog2_0.20_amd64.deb
+sudo dpkg --force-confold -i remote-syslog2_0.20_amd64.deb
 sudo remote_syslog
 
 # install provisioner and dependencies
