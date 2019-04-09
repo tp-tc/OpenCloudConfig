9b363836cab06c6d961110daf0ceee2521c03f7f
Rob Thijssen <rthijssen@gmail.com>
2019-04-09 15:31:55 +0300

    create provisioner service account & grant role allowing assignment of service accounts to provisioned instances

--- ci/gcloud-spawn-provisioner.sh
+++ ci/gcloud-spawn-provisioner.sh
@@ -4,9 +4,19 @@ script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 script_name=$(basename ${0##*/} .sh)
 
 # script config
+project_name=windows-workers
 provisioner_instance_name_prefix=releng-gcp-provisioner
 provisioner_instance_zone=us-east1-b
 provisioner_instance_machine_type=f1-micro
+
+# create a service account for the provisioners if it doesn't exist
+service_account_name=releng-gcp-provisioner
+if [[ "$(gcloud iam service-accounts list --filter name:${service_account_name} --format json)" == "[]" ]]; then
+  gcloud iam service-accounts create ${service_account_name} --display-name "releng gcp provisioner"
+  _echo "created service account: _bold_${service_account_name}_reset_"
+  # grant role allowing assignment of service accounts to provisioned instances
+  gcloud projects add-iam-policy-binding ${project_name} --member serviceAccount:${service_account_name}@${project_name}.iam.gserviceaccount.com --role roles/iam.serviceAccountUser
+fi
 
 # generate a new provisioner instance name which does not pre-exist
 existing_provisioner_instance_uri_list=(`gcloud compute instances list --uri`)
@@ -35,6 +45,7 @@ gcloud compute instances create ${provisioner_instance_name} \
   --zone ${provisioner_instance_zone} \
   --machine-type ${provisioner_instance_machine_type} \
   --scopes compute-rw,service-management,storage-rw \
+  --service-account ${service_account_name} \
   --metadata "^;^startup-script-url=gs://open-cloud-config/gcloud-init-provisioner.sh;livelogSecret=${livelogSecret};livelogcrt=${livelogcrt};livelogkey=${livelogkey};pgpKey=${pgpKey};relengapiToken=${relengapiToken};occInstallersToken=${occInstallersToken}"
 echo "$(tput dim)[${script_name} $(date --utc +"%F %T.%3NZ")]$(tput sgr0) provisioner: ${provisioner_instance_name} created as ${provisioner_instance_machine_type} in ${provisioner_instance_zone}$(tput sgr0)"
 
