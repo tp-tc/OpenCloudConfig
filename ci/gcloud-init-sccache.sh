5fb165194135bb7b5475e676aae411cca84df68d
Rob Thijssen <rthijssen@gmail.com>
2019-04-10 12:15:38 +0300

    add occ bucket access to worker service accounts

--- ci/gcloud-init-sccache.sh
+++ ci/gcloud-init-sccache.sh
@@ -32,11 +32,18 @@ for scm_level in {1..3}; do
   fi
 done
 
+# create grant open-cloud-config bucket viewer access to each service account so that workers can read their startup scripts
+for scm_level in {1..3}; do
+  service_account_name=taskcluster-level-${scm_level}-sccache
+  gsutil iam ch serviceAccount:${service_account_name}@${project_name}.iam.gserviceaccount.com:objectViewer gs://open-cloud-config/
+  _echo "added viewer access for: _bold_${service_account_name}@${project_name}_reset_ to bucket: _bold_gs://open-cloud-config/_reset_"
+done
+
 # get a list of compute regions
 region_uri_list=(`gcloud compute regions list --uri`)
 region_name_list=("${region_uri_list[@]##*/}")
 
-# create regional sccache buckets for each scm level and grant access to the appropriate service account for that level
+# create regional sccache buckets for each scm level and grant bucket access to the appropriate service account for that level
 for region_name in "${region_name_list[@]}"; do
   for scm_level in {1..3}; do
     service_account_name=taskcluster-level-${scm_level}-sccache
@@ -46,7 +53,7 @@ for region_name in "${region_name_list[@]}"; do
       gsutil mb -p ${project_name} -c regional -l ${region_name} gs://${service_account_name}-${region_name}/
       _echo "created bucket: _bold_gs://${service_account_name}-${region_name}/_reset_"
       gsutil iam ch serviceAccount:${service_account_name}@${project_name}.iam.gserviceaccount.com:objectAdmin gs://${service_account_name}-${region_name}/
-      _echo "added access for: _bold_${service_account_name}@${project_name}_reset_ to bucket: _bold_gs://${service_account_name}-${region_name}/_reset_"
+      _echo "added admin access for: _bold_${service_account_name}@${project_name}_reset_ to bucket: _bold_gs://${service_account_name}-${region_name}/_reset_"
     fi
   done
 done
