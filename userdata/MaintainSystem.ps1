86774c87881b305574c94c34cccdfbbd6ee6ce90
Rob Thijssen <rthijssen@gmail.com>
2019-11-08 21:25:57 +0200

    yoga debugging

--- userdata/MaintainSystem.ps1
+++ userdata/MaintainSystem.ps1
@@ -210,11 +210,11 @@ function Invoke-OccReset {
           }
         }
         if (Test-Path -Path ('{0}\Mozilla\OpenCloudConfig\occ-public.key' -f $env:ProgramData) -ErrorAction SilentlyContinue) {
-          Write-Log -message ('{0} :: public key found at: {1}' -f $($MyInvocation.MyCommand.Name), ('{0}\Mozilla\OpenCloudConfig\occ-public.key' -f $env:ProgramData)) -severity 'DEBUG'
+          Write-Log -message ('{0} :: gpg public key found at: {1}' -f $($MyInvocation.MyCommand.Name), ('{0}\Mozilla\OpenCloudConfig\occ-public.key' -f $env:ProgramData)) -severity 'DEBUG'
           $publicKey = (Get-Content -Path ('{0}\Mozilla\OpenCloudConfig\occ-public.key' -f $env:ProgramData) -Raw)
           Write-Log -message ('{0} :: {1}' -f $($MyInvocation.MyCommand.Name), $publicKey) -severity 'DEBUG'
         } else {
-          Write-Log -message ('{0} :: public key not found at: {1}' -f $($MyInvocation.MyCommand.Name), ('{0}\Mozilla\OpenCloudConfig\occ-public.key' -f $env:ProgramData)) -severity 'ERROR'
+          Write-Log -message ('{0} :: gpg public key not found at: {1}' -f $($MyInvocation.MyCommand.Name), ('{0}\Mozilla\OpenCloudConfig\occ-public.key' -f $env:ProgramData)) -severity 'ERROR'
         }
         # todo: generate C:\generic-worker\ed25519-private.key and C:\generic-worker\ed25519-public.key
       }
