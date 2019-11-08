cb5b730ae6f9f1ca3cd44a61822d37e1a902edb5
Rob Thijssen <rthijssen@gmail.com>
2019-11-08 21:24:09 +0200

    yoga debugging

--- userdata/MaintainSystem.ps1
+++ userdata/MaintainSystem.ps1
@@ -213,6 +213,8 @@ function Invoke-OccReset {
           Write-Log -message ('{0} :: public key found at: {1}' -f $($MyInvocation.MyCommand.Name), ('{0}\Mozilla\OpenCloudConfig\occ-public.key' -f $env:ProgramData)) -severity 'DEBUG'
           $publicKey = (Get-Content -Path ('{0}\Mozilla\OpenCloudConfig\occ-public.key' -f $env:ProgramData) -Raw)
           Write-Log -message ('{0} :: {1}' -f $($MyInvocation.MyCommand.Name), $publicKey) -severity 'DEBUG'
+        } else {
+          Write-Log -message ('{0} :: public key not found at: {1}' -f $($MyInvocation.MyCommand.Name), ('{0}\Mozilla\OpenCloudConfig\occ-public.key' -f $env:ProgramData)) -severity 'ERROR'
         }
         # todo: generate C:\generic-worker\ed25519-private.key and C:\generic-worker\ed25519-public.key
       }
