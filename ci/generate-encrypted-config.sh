498e40f90d3eef069ed381f8ddb5fc5ca7fadcdc
Rob Thijssen <rthijssen@gmail.com>
2019-11-09 16:38:25 +0200

    gw configs for yogas

---
+++ ci/generate-encrypted-config.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+temp_dir=$(mktemp -d)
+current_script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+current_script_name=$(basename ${0##*/} .sh)
+
+echo ${temp_dir}
+
+mkdir -p ${temp_dir}/gnupg
+chmod 700 ${temp_dir}/gnupg
+
+accessToken=$(pass Mozilla/TaskCluster/client/project/releng/generic-worker/bitbar-gecko-t-win10-aarch64)
+clientId=project/releng/generic-worker/bitbar-gecko-t-win10-aarch64
+provisionerId=bitbar
+workerGroup=bitbar-sc
+workerType=gecko-t-win64-aarch64-laptop
+requiredDiskSpaceMegabytes=10240
+rootURL=https://firefox-ci-tc.services.mozilla.com
+taskDrive=C
+
+for pub_key_path in ${current_script_dir}/../keys/*; do
+  workerId=$(basename ${pub_key_path})
+  gpg2 --homedir ${temp_dir}/gnupg --import ${pub_key_path}
+  jq --sort-keys \
+    --arg accessToken ${accessToken} \
+    --arg clientId ${clientId} \
+    --arg provisionerId ${provisionerId} \
+    --arg requiredDiskSpaceMegabytes ${requiredDiskSpaceMegabytes} \
+    --arg rootURL ${rootURL} \
+    --arg taskDrive ${taskDrive} \
+    --arg workerGroup ${workerGroup} \
+    --arg workerId ${workerId} \
+    --arg workerType ${workerType} \
+    '. | .accessToken = $accessToken | .clientId = $clientId | .provisionerId = $provisionerId | .requiredDiskSpaceMegabytes = $requiredDiskSpaceMegabytes | .rootURL = $rootURL | .workerGroup = $workerGroup | .workerId = $workerId | .workerType = $workerType | .cachesDir = $taskDrive + ":\\caches" | .downloadsDir = $taskDrive + ":\\downloads" | .tasksDir = $taskDrive + ":\\tasks"' \
+    ${current_script_dir}/../userdata/Configuration/GenericWorker/generic-worker.config > ${current_script_dir}/../cfg/generic-worker/${workerId}.json
+    gpg2 --homedir ${temp_dir}/gnupg --batch --output ${current_script_dir}/../cfg/generic-worker/${workerId}.json.gpg --encrypt --recipient yoga-${workerId/t-lenovoyogac630-/} --trust-model always ${current_script_dir}/../cfg/generic-worker/${workerId}.json
+done
+#gpg2 --homedir ${temp_dir}/gnupg --list-keys
+
+rm -rf ${temp_dir}
